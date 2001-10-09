1dd16da6629897ac6b37bd9c6ec852177095f726
Rhys Weatherley <rweather@southern-storm.com.au>
2001-10-09 04:16:56 +0000

    Verification for "switch" instructions; process branch labels.

--- engine/verify_branch.c
+++ engine/verify_branch.c
@@ -81,18 +81,210 @@ static char const unaryBranchMatrix[ILEngineType_ValidTypes] =
      T_I4, T_I8, T_I,  T_F,  T_M,  T_O,  T_T, T_NO,
 };
 
-#elif defined(IL_VERIFY_LOCALS)
+/*
+ * Information that is stored for a label.
+ */
+typedef struct _tagBranchLabel BranchLabel;
+struct _tagBranchLabel
+{
+	ILUInt32	 address;
+	BranchLabel *next;
+	ILUInt32	 stackSize;
 
-ILUInt32 dest;
+};
 
-#else /* IL_VERIFY_CODE */
-
+/*
+ * Get the destination for a short or long branch instruction.
+ */
 #define	GET_SHORT_DEST()	(offset + 2 + ((ILUInt32)(ILInt32)(pc[1])))
 #define	GET_LONG_DEST()		(offset + 5 + ((ILUInt32)IL_READ_INT32(pc + 1)))
 
+/*
+ * Find an existing branch label information block.
+ */
+static BranchLabel *FindLabel(BranchLabel *labelList, ILUInt32 address)
+{
+	while(labelList != 0)
+	{
+		if(labelList->address == address)
+		{
+			return labelList;
+		}
+		labelList = labelList->next;
+	}
+	return 0;
+}
+
+/*
+ * Validate the current contents of the stack against
+ * information that was previously recorded.
+ */
+static int ValidateStack(BranchLabel *label, ILEngineStackItem *stack,
+						 ILUInt32 stackSize)
+{
+	ILUInt32 posn;
+	ILEngineStackItem *labelStack;
+	if(stackSize != label->stackSize)
+	{
+		return 0;
+	}
+	labelStack = (ILEngineStackItem *)(label + 1);
+	for(posn = 0; posn < stackSize; ++posn)
+	{
+		if(stack[posn].engineType != labelStack[posn].engineType)
+		{
+			return 0;
+		}
+		if(stack[posn].engineType == ILEngineType_M ||
+		   stack[posn].engineType == ILEngineType_T ||
+		   stack[posn].engineType == ILEngineType_MV)
+		{
+			if(!ILTypeIdentical(stack[posn].typeInfo,
+								labelStack[posn].typeInfo))
+			{
+				return 0;
+			}
+		}
+		else if(stack[posn].engineType == ILEngineType_O)
+		{
+			/* The current stack must be a sub-class of the label stack */
+			if(stack[posn].typeInfo == 0)
+			{
+				/* The current stack is "null", so use the label type */
+				stack[posn].typeInfo = labelStack[posn].typeInfo;
+			}
+			else if(labelStack[posn].typeInfo != 0 &&
+			        ILType_IsClass(labelStack[posn].typeInfo) &&
+					IsSubClass(stack[posn].typeInfo,
+							   ILType_ToClass(labelStack[posn].typeInfo)))
+			{
+				/* TODO: may need a better sub-class test above */
+				stack[posn].typeInfo = labelStack[posn].typeInfo;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+/*
+ * Copy the current contents of the stack to a new label block.
+ * Returns NULL if out of memory.
+ */
+static BranchLabel *CopyStack(TempAllocator *allocator, ILUInt32 address,
+							  ILEngineStackItem *stack, ILUInt32 stackSize)
+{
+	BranchLabel *label;
+
+	/* Allocate space for the label */
+	label = (BranchLabel *)TempAllocate
+				(allocator, sizeof(BranchLabel) +
+							sizeof(ILEngineStackItem) * stackSize);
+	if(!label)
+	{
+		return 0;
+	}
+
+	/* Copy the address and stack information to the label */
+	label->address = address;
+	label->stackSize = stackSize;
+	if(stackSize > 0)
+	{
+		ILMemCpy(label + 1, stack, stackSize * sizeof(ILEngineStackItem));
+	}
+
+	/* The label has been initialized */
+	return label;
+}
+
+/*
+ * Reload the contents of the stack from a pre-existing label.
+ * Returns the new stack size.
+ */
+static ILUInt32 ReloadStack(BranchLabel *label, ILEngineStackItem *stack)
+{
+	if(label->stackSize > 0)
+	{
+		ILMemCpy(stack, label + 1,
+				 label->stackSize * sizeof(ILEngineStackItem));
+	}
+	return label->stackSize;
+}
+
+/*
+ * Validate or record the stack information for a destination label.
+ */
 #define	VALIDATE_BRANCH_STACK(dest)	\
 			do { \
+				currLabel = FindLabel(labelList, (dest)); \
+				if(currLabel) \
+				{ \
+					if(!ValidateStack(currLabel, stack, stackSize)) \
+					{ \
+						VERIFY_STACK_ERROR(); \
+					} \
+				} \
+				else \
+				{ \
+					currLabel = CopyStack(&allocator, (dest), \
+										  stack, stackSize); \
+					if(!currLabel) \
+					{ \
+						VERIFY_MEMORY_ERROR(); \
+					} \
+					currLabel->next = labelList; \
+					labelList = currLabel; \
+				} \
 			} while (0)
+
+/*
+ * Validate or record the stack information for a jump target.
+ */
+#define	VALIDATE_TARGET_STACK(address) \
+			do { \
+				currLabel = FindLabel(labelList, (address)); \
+				if(currLabel) \
+				{ \
+					if(lastWasJump) \
+					{ \
+						/* Reload the stack contents from the label */ \
+						stackSize = ReloadStack(currLabel, stack); \
+					} \
+					else if(!ValidateStack(currLabel, stack, stackSize)) \
+					{ \
+						VERIFY_STACK_ERROR(); \
+					} \
+				} \
+				else \
+				{ \
+					if(lastWasJump) \
+					{ \
+						/* This is probably the head of a while loop */ \
+						/* which is assumed to always be zero-sized. */ \
+						stackSize = 0; \
+					} \
+					currLabel = CopyStack(&allocator, (address), \
+										  stack, stackSize); \
+					if(!currLabel) \
+					{ \
+						VERIFY_MEMORY_ERROR(); \
+					} \
+					currLabel->next = labelList; \
+					labelList = currLabel; \
+				} \
+			} while (0)
+
+#elif defined(IL_VERIFY_LOCALS)
+
+ILUInt32 dest;
+ILUInt32 numEntries;
+BranchLabel *labelList;
+BranchLabel *currLabel;
+
+#else /* IL_VERIFY_CODE */
 
 case IL_OP_BR_S:
 {
@@ -268,7 +460,24 @@ case IL_OP_BLT_UN:
 case IL_OP_SWITCH:
 {
 	/* Switch statement */
-	/* TODO */
+	if(STK_UNARY == ILEngineType_I4)
+	{
+		numEntries = IL_READ_UINT32(pc + 1);
+		insnSize = 5 + numEntries * 4;
+		ILCoderSwitchStart(coder, numArgs);
+		for(argNum = 0; argNum < numEntries; ++argNum)
+		{
+			dest = (ILUInt32)((pc + insnSize) - (unsigned char *)(code->code)) +
+				   (ILUInt32)(IL_READ_INT32(pc + 5 + argNum * 4));
+			ILCoderSwitchEntry(coder, dest);
+			VALIDATE_BRANCH_STACK(dest);
+		}
+		--stackSize;
+	}
+	else
+	{
+		VERIFY_TYPE_ERROR();
+	}
 }
 break;
 

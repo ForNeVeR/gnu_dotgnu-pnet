559dccb0637e69ee5ab5af5111a08c4e2c7beb13
Rhys Weatherley <rweather@southern-storm.com.au>
2001-10-12 05:55:46 +0000

    Declare the lookup and method call API's.

--- include/il_engine.h
+++ include/il_engine.h
@@ -38,6 +38,33 @@ typedef struct _tagILExecProcess ILExecProcess;
 typedef struct _tagILExecThread ILExecThread;
 
 /*
+ * Object handle.
+ */
+typedef struct _tagILObject ILObject;
+
+/*
+ * Types that are useful for passing values through va lists.
+ */
+#if SIZEOF_INT > 4
+typedef int				ILVaInt;
+typedef unsigned int	ILVaUInt;
+#else
+typedef ILInt32			ILVaInt;
+typedef ILUInt32		ILVaUInt;
+#endif
+typedef double			ILVaDouble;
+
+/*
+ * The structure that is used to pass typed references around.
+ */
+typedef struct
+{
+	void *type;
+	void *value;
+
+} ILTypedRef;
+
+/*
  * Create a new process, including the "main" thread.
  */
 ILExecProcess *ILExecProcessCreate(void);
@@ -103,6 +130,92 @@ ILMethod *ILExecThreadStackMethod(ILExecThread *thread, unsigned long num);
  * Returns zero if there is something wrong with the type.
  */
 ILUInt32 ILSizeOfType(ILExecThread *thread, ILType *type);
+
+/*
+ * Look up a type name within a particular thread's context.
+ * Returns NULL if the name could not be found.
+ */
+ILClass *ILExecThreadLookupType(ILExecThread *thread, const char *typeName);
+
+/*
+ * Look up a method by type name, method name, and signature.
+ * Returns NULL if the method could not be found.  This function
+ * will search ancestor classes if the method is not found in
+ * a child class.
+ */
+ILMethod *ILExecThreadLookupMethod(ILExecThread *thread,
+								   const char *typeName,
+								   const char *methodName,
+								   const char *signature);
+
+/*
+ * Look up a field by type name, field name, and signature.
+ * Returns NULL if the field could not be found.  This function
+ * will search ancestor classes if the field is not found in
+ * a child class.
+ */
+ILField *ILExecThreadLookupField(ILExecThread *thread,
+								 const char *typeName,
+								 const char *fieldName,
+								 const char *signature);
+
+/*
+ * Call a particular method within a thread's context.
+ * Returns non-zero if an exception was thrown during
+ * the processing of the method.  Zero otherwise.
+ * The return value, if any, is placed in "*result".
+ */
+int ILExecThreadCall(ILExecThread *thread, ILMethod *method,
+					 void *result, ...);
+
+/*
+ * Call a particular virtual method within a thread's context.
+ * If the method isn't marked as virtual, then an ordinary
+ * method call will be used instead.
+ */
+int ILExecThreadCallVirtual(ILExecThread *thread, ILMethod *method,
+							void *result, ...);
+
+/*
+ * Look up a method by name and then call it.  If the
+ * method is not found, an exception will be thrown.
+ */
+int ILExecThreadCallNamed(ILExecThread *thread, const char *typeName,
+						  const char *methodName, const char *signature,
+						  void *result, ...);
+
+/*
+ * Look up a method by name and then call it using a
+ * virtual cal.  If the method is not found, an exception
+ * will be thrown.
+ */
+int ILExecThreadCallNamedVirtual(ILExecThread *thread, const char *typeName,
+						         const char *methodName, const char *signature,
+						         void *result, ...);
+
+/*
+ * Determine if there is a last-occuring exception
+ * for a thread.  Returns non-zero if so.
+ */
+int ILExecThreadHasException(ILExecThread *thread);
+
+/*
+ * Get the last-occurring exception for a thread.
+ * Returns NULL if there is no exception object.
+ */
+ILObject *ILExecThreadGetException(ILExecThread *thread);
+
+/*
+ * Set the last-occuring exception for a thread.
+ */
+void ILExecThreadSetException(ILExecThread *thread, ILObject *obj);
+
+/*
+ * Clear the last-occuring exception for a thread.  If an
+ * exception is not cleared, it will be re-thrown from the
+ * current method.
+ */
+void ILExecThreadClearException(ILExecThread *thread);
 
 #ifdef	__cplusplus
 };

ef16fb5af07dfcd9b5307d46e784095d1deed4bf
Rhys Weatherley <rweather@southern-storm.com.au>
2001-10-18 04:29:20 +0000

    Output call-related code.

--- engine/cvmc_call.c
+++ engine/cvmc_call.c
@@ -34,39 +34,68 @@ static void CVMCoder_DownConvertArg(ILCoder *coder, ILEngineStackItem *args,
 	/* TODO */
 }
 
-static void CVMCoder_NewObj(ILCoder *coder, ILClass *classInfo)
-{
-	/* TODO */
-}
-
-static void CVMCoder_CtorArgs(ILCoder *coder, ILEngineStackItem *args,
-					 	      ILUInt32 numArgs)
-{
-	/* TODO */
-}
-
 static void CVMCoder_ValueCtorArgs(ILCoder *coder, ILClass *classInfo,
 								   ILEngineStackItem *args, ILUInt32 numArgs)
 {
-	/* TODO */
+	ILUInt32 posn = ComputeStackSize(coder, args, numArgs);
+	ILUInt32 size = GetTypeSize(coder->thread, ILType_FromValueType(classInfo));
+	CVM_DWIDE(COP_NEW_VALUE, posn, size);
+	CVM_ADJUST(size + 1);
+}
+
+/*
+ * Adjust the position of the stack for a call.
+ */
+static void AdjustForCall(ILCoder *coder, ILEngineStackItem *args,
+						  ILUInt32 numArgs, ILEngineStackItem *returnItem,
+						  ILMethod *methodInfo)
+{
+	CVM_ADJUST(-(ILInt32)ComputeStackSize(coder, args, numArgs));
+	if(returnItem != 0 && returnItem->engineType != ILEngineType_Invalid)
+	{
+		CVM_ADJUST(ComputeStackSize(coder, returnItem, 1));
+	}
 }
 
 static void CVMCoder_CallMethod(ILCoder *coder, ILEngineStackItem *args,
-					   		    ILUInt32 numArgs, ILMethod *methodInfo)
+					   		    ILUInt32 numArgs,
+								ILEngineStackItem *returnItem,
+								ILMethod *methodInfo)
 {
-	/* TODO */
+	CVM_BYTE(COP_CALL_EXTERN);
+	CVM_WORD(0);
+	CVM_PTR(methodInfo);
+	AdjustForCall(coder, args, numArgs, returnItem, methodInfo);
+}
+
+static void CVMCoder_CallCtor(ILCoder *coder, ILEngineStackItem *args,
+					   		  ILUInt32 numArgs, ILMethod *methodInfo)
+{
+	CVM_BYTE(COP_CALL_CTOR);
+	CVM_WORD(0);
+	CVM_PTR(methodInfo);
+	CVM_ADJUST(-(ILInt32)ComputeStackSize(coder, args, numArgs));
+	CVM_ADJUST(1);
 }
 
 static void CVMCoder_CallVirtual(ILCoder *coder, ILEngineStackItem *args,
-					    	     ILUInt32 numArgs, ILMethod *methodInfo)
+					    	     ILUInt32 numArgs,
+								 ILEngineStackItem *returnItem,
+								 ILMethod *methodInfo)
 {
-	/* TODO */
+	CVM_WIDE(COP_CALL_VIRTUAL, methodInfo->index);
+	AdjustForCall(coder, args, numArgs, returnItem, methodInfo);
 }
 
 static void CVMCoder_CallInterface(ILCoder *coder, ILEngineStackItem *args,
-					      		   ILUInt32 numArgs, ILMethod *methodInfo)
+					      		   ILUInt32 numArgs,
+								   ILEngineStackItem *returnItem,
+								   ILMethod *methodInfo)
 {
-	/* TODO */
+	void *ptr = ILMethod_Owner(methodInfo);
+	CVM_WIDE(COP_CALL_INTERFACE, methodInfo->index);
+	CVM_PTR(ptr);
+	AdjustForCall(coder, args, numArgs, returnItem, methodInfo);
 }
 
 static void CVMCoder_JumpMethod(ILCoder *coder, ILMethod *methodInfo)
@@ -77,7 +106,59 @@ static void CVMCoder_JumpMethod(ILCoder *coder, ILMethod *methodInfo)
 static void CVMCoder_ReturnInsn(ILCoder *coder, ILEngineType engineType,
 							    ILType *returnType)
 {
-	/* TODO */
+	switch(engineType)
+	{
+		case ILEngineType_Invalid:
+		{
+			CVM_BYTE(COP_RETURN);
+		}
+		break;
+
+		case ILEngineType_M:
+		case ILEngineType_O:
+		case ILEngineType_T:
+		case ILEngineType_I4:
+	#ifdef IL_NATIVE_INT32
+		case ILEngineType_I:
+	#endif
+		{
+			CVM_BYTE(COP_RETURN_1);
+			CVM_ADJUST(-1);
+		}
+		break;
+
+		case ILEngineType_I8:
+	#ifdef IL_NATIVE_INT64
+		case ILEngineType_I:
+	#endif
+		{
+			CVM_RETURN(CVM_WORDS_PER_LONG);
+			CVM_ADJUST(-CVM_WORDS_PER_LONG);
+		}
+		break;
+
+		case ILEngineType_F:
+		{
+			CVM_RETURN(CVM_WORDS_PER_NATIVE_FLOAT);
+			CVM_ADJUST(-CVM_WORDS_PER_NATIVE_FLOAT);
+		}
+		break;
+
+		case ILEngineType_MV:
+		{
+			ILUInt32 size = GetTypeSize(coder->thread, returnType);
+			CVM_RETURN(size);
+			CVM_ADJUST(-(ILInt32)size);
+		}
+		break;
+
+		case ILEngineType_TypedRef:
+		{
+			CVM_RETURN(CVM_WORDS_PER_TYPED_REF);
+			CVM_ADJUST(-CVM_WORDS_PER_TYPED_REF);
+		}
+		break;
+	}
 }
 
 #endif	/* IL_CVMC_CODE */

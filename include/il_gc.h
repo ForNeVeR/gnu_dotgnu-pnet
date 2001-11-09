dbbb71e69e6aaed84e8281b3eac5e53411d37073
Rhys Weatherley <rweather@southern-storm.com.au>
2001-11-09 04:14:13 +0000

    Implement the garbage collection API in terms of the Hans-Boehm collector.

---
+++ include/il_gc.h
@@ -0,0 +1,80 @@
+/*
+ * il_gc.h - Interface to the garbage collector.
+ *
+ * Copyright (C) 2001  Southern Storm Software, Pty Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef	_IL_GC_H
+#define	_IL_GC_H
+
+#include "il_system.h"
+
+#ifdef	__cplusplus
+extern	"C" {
+#endif
+
+/*
+ * Initialize the garbage collector with a specific maximum heap size.
+ * If "maxSize" is zero, then the collector will use all of memory.
+ */
+void ILGCInit(unsigned long maxSize);
+
+/*
+ * Allocate a block of memory from the garbage collector.
+ * The block may contain pointers to other blocks.  The
+ * block is guaranteed to be zero'ed.
+ */
+void *ILGCAlloc(unsigned long size);
+
+/*
+ * Allocate a block of memory from the garbage collector
+ * that will never contain pointers.  Use this for allocating
+ * arrays of atomic numeric types.  The block is zero'ed.
+ */
+void *ILGCAllocAtomic(unsigned long size);
+
+/*
+ * Finalization callback function.
+ */
+typedef void (*ILGCFinalizer)(void *block, void *data);
+
+/*
+ * Register a finalizer for a block of memory.
+ */
+void ILGCRegisterFinalizer(void *block, ILGCFinalizer func, void *data);
+
+/*
+ * Mark a region of memory as not containing any object pointers.
+ */
+void ILGCMarkNoPointers(void *start, unsigned long size);
+
+/*
+ * Trigger explicit garbage collection.
+ */
+void ILGCCollect(void);
+
+/*
+ * Perform a little bit of garbage collection.
+ * Returns zero if nothing more to do.
+ */
+int ILGCCollectALittle(void);
+
+#ifdef	__cplusplus
+};
+#endif
+
+#endif	/* _IL_GC_H */
---
+++ support/hb_gc.c
@@ -0,0 +1,75 @@
+/*
+ * hb_gc.c - Interface to the Hans-Boehm garbage collector.
+ *
+ * Copyright (C) 2001  Southern Storm Software, Pty Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "il_gc.h"
+#include "../libgc/include/gc.h"
+
+#ifdef	__cplusplus
+extern	"C" {
+#endif
+
+void ILGCInit(unsigned long maxSize)
+{
+	GC_INIT();
+	GC_set_max_heap_size((size_t)maxSize);
+}
+
+void *ILGCAlloc(unsigned long size)
+{
+	/* The Hans-Boehm routines guarantee to zero the block */
+	return GC_MALLOC((size_t)size);
+}
+
+void *ILGCAllocAtomic(unsigned long size)
+{
+	void *block = GC_MALLOC_ATOMIC((size_t)size);
+	if(block)
+	{
+		/* The Hans-Boehm routines don't guarantee to zero the block */
+		ILMemZero(block, size);
+	}
+	return block;
+}
+
+void ILGCRegisterFinalizer(void *block, ILGCFinalizer func, void *data)
+{
+	/* We use the Java-style finalization algorithm, which
+	   ignores cycles in the object structure */
+	GC_REGISTER_FINALIZER_NO_ORDER(block, func, data, 0, 0);
+}
+
+void ILGCMarkNoPointers(void *start, unsigned long size)
+{
+	GC_exclude_static_roots(start, (void *)(((unsigned char *)start) + size));
+}
+
+void ILGCCollect(void)
+{
+	GC_gcollect();
+}
+
+int ILGCCollectALittle(void)
+{
+	return GC_collect_a_little();
+}
+
+#ifdef	__cplusplus
+};
+#endif

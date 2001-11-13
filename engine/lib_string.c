c5d2aa7faf81876fa8fdb49ecc5d01d67fc209d1
Rhys Weatherley <rweather@southern-storm.com.au>
2001-11-13 02:07:32 +0000

    Move the external string API's out of "strapi.c" and into "lib_string.c"; then implement them efficiently in terms of the existing "internalcall" representation.

--- engine/lib_string.c
+++ engine/lib_string.c
@@ -1,5 +1,5 @@
 /*
- * string.c - Internalcall methods for "System.String".
+ * lib_string.c - Internalcall methods for "System.String".
  *
  * Copyright (C) 2001  Southern Storm Software, Pty Ltd.
  *
@@ -941,6 +941,309 @@ IL_METHOD_BEGIN(_ILSystemStringMethods)
 	IL_METHOD("TrimHelper",	 "(T[ci)oSystem.String;", System_String_TrimHelper)
 	IL_METHOD("InternalGetChar", "(Ti)c", System_String_InternalGetChar)
 IL_METHOD_END
+
+ILString *ILStringCreate(ILExecThread *thread, const char *str)
+{
+	if(str)
+	{
+		return ILStringCreateLen(thread, str, strlen(str));
+	}
+	else
+	{
+		return (ILString *)0;
+	}
+}
+
+ILString *ILStringCreateLen(ILExecThread *thread, const char *str, int len)
+{
+	if(str && len >= 0)
+	{
+		/* Call the "String(sbyte *, int, int, Encoding)" constructor */
+		return (ILString *)System_String_ctor_6
+					(thread, (ILInt8 *)str, 0, (ILInt32)len, (void *)0);
+	}
+	else
+	{
+		return (ILString *)0;
+	}
+}
+
+ILString *ILStringWCreate(ILExecThread *thread, const ILUInt16 *str)
+{
+	if(str)
+	{
+		int len = 0;
+		while(str[len] != 0)
+		{
+			++len;
+		}
+		return ILStringWCreateLen(thread, str, len);
+	}
+	else
+	{
+		return (ILString *)0;
+	}
+}
+
+ILString *ILStringWCreateLen(ILExecThread *thread,
+							 const ILUInt16 *str, int len)
+{
+	if(str && len >= 0)
+	{
+		/* Call the "String(char *, int, int)" constructor */
+		return (ILString *)System_String_ctor_4
+				(thread, (ILUInt16 *)str, 0, (ILInt32)len);
+	}
+	else
+	{
+		return (ILString *)0;
+	}
+}
+
+int ILStringCompare(ILExecThread *thread, ILString *strA, ILString *strB)
+{
+	ILInt32 result = 0;
+	ILExecThreadCallNamed(thread, "System.String", "Compare",
+						  "(oSystem.String;oSystem.String;)i",
+						  &result, strA, strB);
+	return result;
+}
+
+int ILStringCompareIgnoreCase(ILExecThread *thread, ILString *strA,
+							  ILString *strB)
+{
+	ILInt32 result = 0;
+	ILExecThreadCallNamed(thread, "System.String", "Compare",
+						  "(oSystem.String;oSystem.String;Z)i",
+						  &result, strA, strB, (ILVaInt)0);
+	return result;
+}
+
+int ILStringCompareOrdinal(ILExecThread *thread, ILString *strA,
+						   ILString *strB)
+{
+	ILUInt16 *bufA;
+	ILUInt16 *bufB;
+	ILInt32 lenA;
+	ILInt32 lenB;
+
+	/* Handle the easy cases first */
+	if(!strA)
+	{
+		if(strB)
+		{
+			return -1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	else if(!strB)
+	{
+		return 1;
+	}
+
+	/* Scan the buffers looking for differences */
+	bufA = StringToBuffer(strA);
+	lenA = ((System_String *)strA)->length;
+	bufB = StringToBuffer(strB);
+	lenB = ((System_String *)strB)->length;
+	while(lenA > 0 && lenB > 0)
+	{
+		if(*bufA < *bufB)
+		{
+			return -1;
+		}
+		else if(*bufA > *bufB)
+		{
+			return 1;
+		}
+		++bufA;
+		++bufB;
+		--lenA;
+		--lenB;
+	}
+
+	/* Determine the ordering based on the tails */
+	if(lenA > 0)
+	{
+		return 1;
+	}
+	else if(lenB > 0)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int ILStringEquals(ILExecThread *thread, ILString *strA, ILString *strB)
+{
+	ILUInt16 *bufA;
+	ILUInt16 *bufB;
+	ILInt32 lenA;
+	ILInt32 lenB;
+
+	/* Handle the easy cases first */
+	if(!strA)
+	{
+		return (strB == 0);
+	}
+	else if(!strB)
+	{
+		return 0;
+	}
+
+	/* Scan the buffers looking for differences */
+	lenA = ((System_String *)strA)->length;
+	lenB = ((System_String *)strB)->length;
+	if(lenA != lenB)
+	{
+		return 0;
+	}
+	else if(!lenA)
+	{
+		return 1;
+	}
+	else
+	{
+		bufA = StringToBuffer(strA);
+		bufB = StringToBuffer(strB);
+		return (ILMemCmp(bufA, bufB, lenA * sizeof(ILUInt16)) == 0);
+	}
+}
+
+ILString *ILStringConcat(ILExecThread *thread, ILString *strA, ILString *strB)
+{
+	if(!strA || ((System_String *)strA)->length == 0)
+	{
+		if(!strB || ((System_String *)strB)->length == 0)
+		{
+			if(strB)
+			{
+				return strB;
+			}
+			else if(strA)
+			{
+				return strA;
+			}
+			else
+			{
+				return (ILString *)AllocString(thread, 0);
+			}
+		}
+		else
+		{
+			return strB;
+		}
+	}
+	else if(!strB || ((System_String *)strB)->length == 0)
+	{
+		return strA;
+	}
+	else
+	{
+		ILInt32 lenA = ((System_String *)strA)->length;
+		ILInt32 lenB = ((System_String *)strB)->length;
+		System_String *result;
+		if(((ILUInt32)(lenA + lenB)) >= ((ILUInt32)(IL_MAX_INT32 / 2)))
+		{
+			/* The resulting string is too big */
+			ILExecThreadThrowOutOfMemory(thread);
+			return 0;
+		}
+		result = AllocString(thread, lenA + lenB);
+		if(result)
+		{
+			ILMemCpy(StringToBuffer(result), StringToBuffer(strA), lenA);
+			ILMemCpy(StringToBuffer(result) + lenA, StringToBuffer(strB), lenB);
+		}
+		return (ILString *)result;
+	}
+}
+
+ILString *ILObjectToString(ILExecThread *thread, ILObject *object)
+{
+	ILString *result = 0;
+	if(object)
+	{
+		/* Call the virtual "ToString" method on the object */
+		ILExecThreadCallNamedVirtual(thread, "System.Object", "ToString",
+									 "(T)oSystem.String;",
+									 &result, object);
+	}
+	return result;
+}
+
+ILString *ILStringIntern(ILExecThread *thread, ILString *str)
+{
+	return (ILString *)System_String_Intern(thread, (System_String *)str);
+}
+
+ILString *_ILStringInternFromImage(ILExecThread *thread, ILImage *image,
+								   ILToken token)
+{
+	const char *str;
+	unsigned long len;
+	System_String *newStr;
+
+	/* Get the string from the image's "#US" blob */
+	str = ILImageGetUserString(image, token & ~IL_META_TOKEN_MASK, &len);
+	if(!str)
+	{
+		/* Shouldn't happen, but intern an empty string anyway */
+		len = 0;
+	}
+
+	/* TODO: look up the intern'ed string hash table */
+
+	/* Allocate space for the string */
+	newStr = AllocString(thread, (ILInt32)len);
+	if(!newStr)
+	{
+		return 0;
+	}
+
+	/* Copy the image data into the string */
+#if defined(__i386) || defined(__i386__)
+	/* We can take a short-cut on x86 platforms which already
+	   has the string in the correct format */
+	if(len > 0)
+	{
+		ILMemCpy(StringToBuffer(newStr), str, len * sizeof(ILUInt16));
+	}
+#else
+	{
+		ILUInt16 *dest = StringToBuffer(newStr);
+		while(len > 0)
+		{
+			*dest++ = IL_READ_UINT16(str);
+			str += 2;
+			--len;
+		}
+	}
+#endif
+
+	/* Return the final string to the caller */
+	return (ILString *)newStr;
+}
+
+ILInt32 _ILStringToBuffer(ILExecThread *thread, ILString *str, ILUInt16 **buf)
+{
+	if(str)
+	{
+		*buf = StringToBuffer(str);
+		return ((System_String *)str)->length;
+	}
+	else
+	{
+		*buf = 0;
+		return 0;
+	}
+}
 
 #ifdef	__cplusplus
 };

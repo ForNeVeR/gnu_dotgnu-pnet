4c3a8cac3eeb1b62eab76bce311c70e1daf386f2
Rhys Weatherley <rweather@southern-storm.com.au>
2001-11-27 23:11:07 +0000

    New class.

---
+++ runtime/System/Security/SecurityException.cs
@@ -0,0 +1,49 @@
+/*
+ * SecurityException.cs - Implementation of the
+ *		"System.Security.SecurityException" class.
+ *
+ * Copyright (C) 2001  Southern Storm Software, Pty Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+namespace System.Security
+{
+
+using System;
+
+public class SecurityException : SystemException
+{
+	// Constructors.
+	public SecurityException()
+			: base(Environment.GetResourceString("Exception_Security")) {}
+	public SecurityException(String msg)
+			: base(msg) {}
+	public SecurityException(String msg, Exception inner)
+			: base(msg, inner) {}
+
+	// Get the default message to use for this exception type.
+	protected internal override String MessageDefault
+			{
+				get
+				{
+					return Environment.GetResourceString
+						("Exception_Security");
+				}
+			}
+
+}; // class SecurityException
+
+}; // namespace System.Security

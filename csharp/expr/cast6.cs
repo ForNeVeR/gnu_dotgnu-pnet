0e78dbbd6b4e68961b9017f887520c6376c5819e
Rhys Weatherley <rweather@southern-storm.com.au>
2002-01-16 04:51:27 +0000

    More tests for successful reference casts and coercions.

--- csharp/expr/cast6.cs
+++ csharp/expr/cast6.cs
@@ -33,6 +33,16 @@ interface K : I, J
 	void m3();
 }
 
+interface L
+{
+	void m4();
+}
+
+struct X
+{
+	int x;
+}
+
 class Test
 {
 	public void m1()
@@ -40,12 +50,16 @@ class Test
 		object o1;
 		Test t1;
 		Test2 t2;
+		Test3 t3;
 		I i1;
 		J j1;
 		K k1;
+		L l1;
+		X x1;
 		object[] ao1;
 		Test[] at1;
 		I[] ai1;
+		X[] ax1;
 
 		// "null" can be coerced to any reference type.
 		o1 = null;
@@ -54,6 +68,25 @@ class Test
 		ao1 = null;
 		at1 = null;
 		ai1 = null;
+		ax1 = null;
+
+		// Any reference type can be coerced to "Object".
+		o1 = t1;
+		o1 = t2;
+		o1 = i1;
+		o1 = ao1;
+		o1 = at1;
+		o1 = ai1;
+		o1 = ax1;
+
+		// "Object" can be cast to any reference type.
+		t1 = (Test)o1;
+		t2 = (Test2)o1;
+		i1 = (I)o1;
+		ao1 = (object[])o1;
+		at1 = (Test[])o1;
+		ai1 = (I[])o1;
+		ax1 = (X[])o1;
 
 		// Coercions are allowed up the inheritance tree.
 		// Casts are allowed down the inheritance tree.
@@ -66,13 +99,33 @@ class Test
 		// Coercions are allowed to implemented interfaces.
 		i1 = t2;
 
+		// Cast to an interface.
+		i1 = (I)t1;
+
 		// Coercions are allowed amongst derived interfaces.
 		i1 = j1;
 		i1 = k1;
 		j1 = k1;
+
+		// Explicit conversion from an interface to an implementing class.
+		t2 = (Test2)j1;		// Something inheriting from Test2 may implement.
+		t3 = (Test3)j1;
+
+		// Explicit conversion between unrelated interfaces.
+		l1 = (L)k1;
+
+		// Coerce or cast between arrays of references.
+		ao1 = at1;
+		ao1 = ai1;
+		at1 = (Test[])ao1;
+		ai1 = (I[])at1;
 	}
 }
 
 class Test2 : Test, I
+{
+}
+
+sealed class Test3 : J
 {
 }
--- csharp/expr/cast6.jl
+++ csharp/expr/cast6.jl
@@ -25,22 +25,73 @@
 {
 } // method m3
 } // class K
+.class private auto interface ansi 'L'
+{
+.method public virtual hidebysig abstract instance void 'm4'() cil managed java 
+{
+} // method m4
+} // class L
+.class private sequential value sealed serializable ansi 'X' extends ['.library']'System'.'ValueType'
+{
+.field private int32 'x'
+} // class X
 .class private auto ansi 'Test' extends ['.library']'System'.'Object'
 {
 .method public hidebysig instance void 'm1'() cil managed java 
 {
+	new	"X"
+	dup
+	invokespecial	"X" "<init>" "()V"
+	astore	9
 	aconst_null
 	astore_1
 	aconst_null
 	astore_2
 	aconst_null
-	astore	4
+	astore	5
 	aconst_null
-	astore	7
+	astore	10
 	aconst_null
-	astore	8
+	astore	11
 	aconst_null
-	astore	9
+	astore	12
+	aconst_null
+	astore	13
+	aload_2
+	astore_1
+	aload_3
+	astore_1
+	aload	5
+	astore_1
+	aload	10
+	astore_1
+	aload	11
+	astore_1
+	aload	12
+	astore_1
+	aload	13
+	astore_1
+	aload_1
+	checkcast	'Test'
+	astore_2
+	aload_1
+	checkcast	'Test2'
+	astore_3
+	aload_1
+	checkcast	'I'
+	astore	5
+	aload_1
+	checkcast	class ['.library']'System'.'Object'[]
+	astore	10
+	aload_1
+	checkcast	class 'Test'[]
+	astore	11
+	aload_1
+	checkcast	class 'I'[]
+	astore	12
+	aload_1
+	checkcast	valuetype 'X'[]
+	astore	13
 	aload_2
 	astore_1
 	aload_3
@@ -55,18 +106,43 @@
 	checkcast	'Test2'
 	astore_3
 	aload_3
-	astore	4
-	aload	5
-	astore	4
-	aload	6
-	astore	4
+	astore	5
+	aload_2
+	checkcast	'I'
+	astore	5
 	aload	6
 	astore	5
+	aload	7
+	astore	5
+	aload	7
+	astore	6
+	aload	6
+	checkcast	'Test2'
+	astore_3
+	aload	6
+	checkcast	'Test3'
+	astore	4
+	aload	7
+	checkcast	'L'
+	astore	8
+	aload	11
+	astore	10
+	aload	12
+	astore	10
+	aload	10
+	checkcast	class 'Test'[]
+	astore	11
+	aload	11
+	checkcast	class 'I'[]
+	astore	12
 	return
-	.locals 10
-	.maxstack 1
+	.locals 14
+	.maxstack 2
 } // method m1
 } // class Test
 .class private auto ansi 'Test2' extends 'Test' implements 'I'
 {
 } // class Test2
+.class private auto sealed ansi 'Test3' extends ['.library']'System'.'Object' implements 'J'
+{
+} // class Test3

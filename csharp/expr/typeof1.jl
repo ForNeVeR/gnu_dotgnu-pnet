9959fb0f4bff5913b8d6afcb9cccd6e7dd1bc544
Rhys Weatherley <rweather@southern-storm.com.au>
2002-01-17 00:24:29 +0000

    New JVM output file for the "typeof1" test.

--- csharp/expr/typeof1.jl
+++ csharp/expr/typeof1.jl
@@ -11,12 +11,20 @@
 {
 .method private hidebysig instance void 'm1'() cil managed java 
 {
+	ldc	"System.Int32"
+	invokestatic	"System/Type" "GetType" "(LSystem/String;)LSystem/Type;"
 	astore_1
+	ldc	"System.Void"
+	invokestatic	"System/Type" "GetType" "(LSystem/String;)LSystem/Type;"
 	astore_1
+	ldc	"int[]"
+	invokestatic	"System/Type" "GetType" "(LSystem/String;)LSystem/Type;"
 	astore_1
+	ldc	"Test[]"
+	invokestatic	"System/Type" "GetType" "(LSystem/String;)LSystem/Type;"
 	astore_1
 	return
 	.locals 2
-	.maxstack 0
+	.maxstack 1
 } // method m1
 } // class Test

f38b01a8f2b2a01fb8266504714a563719fbc8b9
Peter Minten <silvernerd@users.sf.net>
2002-03-30 10:13:03 +0000

    Added empty classes to work around compiler errors.

---
+++ System/Net/SocketAddress.cs
@@ -0,0 +1,70 @@
+/*
+ * SocketAddress.cs - Implementation of the "System.Net.SocketAddress" class.
+ *
+ * Copyright (C) 2002  Southern Storm Software, Pty Ltd.
+ *
+ * This program is free software, you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+namespace System.Net
+{
+
+using System;
+using System.Net.Sockets;
+
+public class SocketAddress
+{
+	[TODO]
+	public SocketAddress(AddressFamily family)
+	{
+	
+	}
+	
+	[TODO]
+	public SocketAddress(AddressFamily family, int size)
+	{
+	
+	}
+	
+	[TODO]
+	public override bool Equals(object comparand)
+	{
+		
+	}
+	
+	[TODO]
+	public override int GetHashCode()
+	{
+	
+	}
+	
+	[TODO]
+	public override String ToString()
+	{
+		return 0;
+	}
+	
+	[TODO]
+	public AddressFamily Family 
+			{ 
+				get
+				{
+					return 0;
+				}
+			
+			}
+}; //class SocketAddress
+
+}; //namespace System.Net

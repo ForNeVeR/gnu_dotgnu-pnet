7fe849cd145e08ebf2622764516433129d7e7eb9
Rhys Weatherley <rweather@southern-storm.com.au>
2002-06-09 01:39:52 +0000

    New output files, after bug fixes to "switch" code generation.

--- csharp/stmt/switch1.jl
+++ csharp/stmt/switch1.jl
@@ -33,203 +33,213 @@
 	dup
 	iconst_2
 	if_icmpeq	?L3
+	dup
 	iconst_3
 	if_icmpeq	?L4
 	goto	?L5
 ?L1:
+	pop
 	iconst_0
 	istore_2
-	goto	?L5
+	goto	?L6
 ?L2:
+	pop
 	iconst_1
 	istore_2
-	goto	?L5
+	goto	?L6
 ?L3:
+	pop
 	iconst_2
 	istore_2
-	goto	?L5
+	goto	?L6
 ?L4:
+	pop
 	iconst_3
 	istore_2
-	goto	?L5
+	goto	?L6
 ?L5:
+	pop
+?L6:
 	iload_1
 	tableswitch (
-		?L6,
 		?L7,
 		?L8,
 		?L9,
 		?L10,
 		?L11,
 		?L12,
-		?L13
+		?L13,
+		?L14
 	)
-	goto	?L14
-?L6:
+	goto	?L15
+?L7:
 	iconst_0
 	istore_2
-	goto	?L14
-?L7:
+	goto	?L16
+?L8:
 	iconst_1
 	istore_2
-	goto	?L14
-?L8:
+	goto	?L16
+?L9:
 	iconst_2
 	istore_2
-	goto	?L14
-?L9:
+	goto	?L16
+?L10:
 	iconst_3
 	istore_2
-	goto	?L14
-?L10:
+	goto	?L16
+?L11:
 	iconst_4
 	istore_2
-	goto	?L14
-?L11:
+	goto	?L16
+?L12:
 	iconst_5
 	istore_2
-	goto	?L14
-?L12:
+	goto	?L16
+?L13:
 	bipush	6
 	istore_2
-	goto	?L14
-?L13:
+	goto	?L16
+?L14:
 	bipush	7
 	istore_2
-	goto	?L14
-?L14:
+	goto	?L16
+?L15:
+?L16:
 	iload_1
 	iconst_1
 	isub
 	tableswitch (
-		?L15,
-		?L16,
 		?L17,
 		?L18,
 		?L19,
 		?L20,
 		?L21,
-		?L22
+		?L22,
+		?L23,
+		?L24
 	)
-	goto	?L23
-?L15:
+	goto	?L25
+?L17:
 	iconst_1
 	istore_2
-	goto	?L23
-?L16:
+	goto	?L26
+?L18:
 	iconst_2
 	istore_2
-	goto	?L23
-?L17:
+	goto	?L26
+?L19:
 	iconst_3
 	istore_2
-	goto	?L23
-?L18:
+	goto	?L26
+?L20:
 	iconst_4
 	istore_2
-	goto	?L23
-?L19:
+	goto	?L26
+?L21:
 	iconst_5
 	istore_2
-	goto	?L23
-?L20:
+	goto	?L26
+?L22:
 	bipush	6
 	istore_2
-	goto	?L23
-?L21:
+	goto	?L26
+?L23:
 	bipush	7
 	istore_2
-	goto	?L23
-?L22:
+	goto	?L26
+?L24:
 	bipush	8
 	istore_2
-	goto	?L23
-?L23:
+	goto	?L26
+?L25:
+?L26:
 	iload_1
 	lookupswitch (
-		100 : ?L24,
-		200 : ?L25,
-		300 : ?L26,
-		400 : ?L27,
-		500 : ?L28,
-		600 : ?L29,
-		700 : ?L30,
-		800 : ?L31,
-		900 : ?L32,
-		1000 : ?L33,
-		1100 : ?L34,
-		1200 : ?L35,
-		1300 : ?L36,
-		1400 : ?L37,
-		1500 : ?L38,
-		1600 : ?L39
+		100 : ?L27,
+		200 : ?L28,
+		300 : ?L29,
+		400 : ?L30,
+		500 : ?L31,
+		600 : ?L32,
+		700 : ?L33,
+		800 : ?L34,
+		900 : ?L35,
+		1000 : ?L36,
+		1100 : ?L37,
+		1200 : ?L38,
+		1300 : ?L39,
+		1400 : ?L40,
+		1500 : ?L41,
+		1600 : ?L42
 	)
-	goto	?L40
-?L39:
+	goto	?L43
+?L42:
 	sipush	1600
 	istore_2
-	goto	?L40
-?L38:
+	goto	?L44
+?L41:
 	sipush	1500
 	istore_2
-	goto	?L40
-?L37:
+	goto	?L44
+?L40:
 	sipush	1400
 	istore_2
-	goto	?L40
-?L36:
+	goto	?L44
+?L39:
 	sipush	1300
 	istore_2
-	goto	?L40
-?L35:
+	goto	?L44
+?L38:
 	sipush	1200
 	istore_2
-	goto	?L40
-?L34:
+	goto	?L44
+?L37:
 	sipush	1100
 	istore_2
-	goto	?L40
-?L33:
+	goto	?L44
+?L36:
 	sipush	1000
 	istore_2
-	goto	?L40
-?L32:
+	goto	?L44
+?L35:
 	sipush	900
 	istore_2
-	goto	?L40
-?L31:
+	goto	?L44
+?L34:
 	sipush	800
 	istore_2
-	goto	?L40
-?L30:
+	goto	?L44
+?L33:
 	sipush	700
 	istore_2
-	goto	?L40
-?L29:
+	goto	?L44
+?L32:
 	sipush	600
 	istore_2
-	goto	?L40
-?L28:
+	goto	?L44
+?L31:
 	sipush	500
 	istore_2
-	goto	?L40
-?L27:
+	goto	?L44
+?L30:
 	sipush	400
 	istore_2
-	goto	?L40
-?L26:
+	goto	?L44
+?L29:
 	sipush	300
 	istore_2
-	goto	?L40
-?L25:
+	goto	?L44
+?L28:
 	sipush	200
 	istore_2
-	goto	?L40
-?L24:
+	goto	?L44
+?L27:
 	bipush	100
 	istore_2
-	goto	?L40
-?L40:
+	goto	?L44
+?L43:
+?L44:
 	return
 	.locals 3
 	.maxstack 3
@@ -240,116 +250,135 @@
 	dup2
 	lconst_0
 	lcmp
-	ifeq	?L41
+	ifeq	?L45
 	dup2
 	lconst_1
 	lcmp
-	ifeq	?L42
+	ifeq	?L46
 	dup2
 	iconst_2
 	i2l
 	lcmp
-	ifeq	?L43
+	ifeq	?L47
+	dup2
 	iconst_3
 	i2l
 	lcmp
-	ifeq	?L44
-	goto	?L45
-?L41:
+	ifeq	?L48
+	goto	?L49
+?L45:
+	pop2
 	iconst_0
 	istore_3
-	goto	?L45
-?L42:
+	goto	?L50
+?L46:
+	pop2
 	iconst_1
 	istore_3
-	goto	?L45
-?L43:
+	goto	?L50
+?L47:
+	pop2
 	iconst_2
 	istore_3
-	goto	?L45
-?L44:
+	goto	?L50
+?L48:
+	pop2
 	iconst_3
 	istore_3
-	goto	?L45
-?L45:
+	goto	?L50
+?L49:
+	pop2
+?L50:
 	lload_1
 	dup2
 	iconst_4
 	i2l
 	lcmp
-	ifge	?L46
+	ifge	?L51
 	dup2
 	lconst_0
 	lcmp
-	ifeq	?L47
+	ifeq	?L52
 	dup2
 	lconst_1
 	lcmp
-	ifeq	?L48
-	dup2
-	iconst_2
-	i2l
-	lcmp
-	ifeq	?L49
-	iconst_3
-	i2l
-	lcmp
-	ifeq	?L50
-	goto	?L51
-?L46:
-	dup2
-	iconst_4
-	i2l
-	lcmp
-	ifeq	?L52
-	dup2
-	iconst_5
-	i2l
-	lcmp
 	ifeq	?L53
 	dup2
-	bipush	6
+	iconst_2
 	i2l
 	lcmp
 	ifeq	?L54
-	bipush	7
+	dup2
+	iconst_3
 	i2l
 	lcmp
 	ifeq	?L55
-	goto	?L51
-?L47:
+	goto	?L56
+?L51:
+	dup2
+	iconst_4
+	i2l
+	lcmp
+	ifeq	?L57
+	dup2
+	iconst_5
+	i2l
+	lcmp
+	ifeq	?L58
+	dup2
+	bipush	6
+	i2l
+	lcmp
+	ifeq	?L59
+	dup2
+	bipush	7
+	i2l
+	lcmp
+	ifeq	?L60
+	goto	?L56
+?L52:
+	pop2
 	iconst_0
 	istore_3
-	goto	?L51
-?L48:
+	goto	?L61
+?L53:
+	pop2
 	iconst_1
 	istore_3
-	goto	?L51
-?L49:
+	goto	?L61
+?L54:
+	pop2
 	iconst_2
 	istore_3
-	goto	?L51
-?L50:
+	goto	?L61
+?L55:
+	pop2
 	iconst_3
 	istore_3
-	goto	?L51
-?L52:
+	goto	?L61
+?L57:
+	pop2
 	iconst_4
 	istore_3
-	goto	?L51
-?L53:
+	goto	?L61
+?L58:
+	pop2
 	iconst_5
 	istore_3
-	goto	?L51
-?L54:
+	goto	?L61
+?L59:
+	pop2
 	bipush	6
 	istore_3
-	goto	?L51
-?L55:
+	goto	?L61
+?L60:
+	pop2
 	bipush	7
 	istore_3
-	goto	?L51
-?L51:
+	goto	?L61
+?L56:
+	pop2
+?L61:
 	return
 	.locals 4
 	.maxstack 6
@@ -357,46 +386,6 @@
 .method private hidebysig instance void 'm3'(class ['.library']'System'.'String' 'x') cil managed java 
 {
 	aload_1
-	dup
-	ifnull	?L56
-	dup
-	ldc	"1"
-	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
-	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
-	ifne	?L57
-	dup
-	ldc	"2"
-	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
-	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
-	ifne	?L58
-	ldc	"3"
-	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
-	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
-	ifne	?L59
-	goto	?L60
-?L56:
-	iconst_0
-	istore_2
-	goto	?L60
-?L57:
-	iconst_1
-	istore_2
-	goto	?L60
-?L58:
-	iconst_2
-	istore_2
-	goto	?L60
-?L59:
-	iconst_3
-	istore_2
-	goto	?L60
-?L60:
-	aload_1
-	dup
-	ldc	"4"
-	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
-	invokestatic	"System/String" "CompareOrdinal" "(LSystem/String;LSystem/String;)I"
-	ifge	?L61
 	dup
 	ifnull	?L62
 	dup
@@ -409,65 +398,124 @@
 	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
 	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
 	ifne	?L64
+	dup
 	ldc	"3"
 	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
 	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
 	ifne	?L65
 	goto	?L66
-?L61:
+?L62:
+	pop
+	iconst_0
+	istore_2
+	goto	?L67
+?L63:
+	pop
+	iconst_1
+	istore_2
+	goto	?L67
+?L64:
+	pop
+	iconst_2
+	istore_2
+	goto	?L67
+?L65:
+	pop
+	iconst_3
+	istore_2
+	goto	?L67
+?L66:
+	pop
+?L67:
+	aload_1
+	dup
+	ldc	"4"
+	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
+	invokestatic	"System/String" "CompareOrdinal" "(LSystem/String;LSystem/String;)I"
+	ifge	?L68
+	dup
+	ifnull	?L69
+	dup
+	ldc	"1"
+	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
+	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
+	ifne	?L70
+	dup
+	ldc	"2"
+	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
+	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
+	ifne	?L71
+	dup
+	ldc	"3"
+	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
+	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
+	ifne	?L72
+	goto	?L73
+?L68:
 	dup
 	ldc	"4"
 	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
 	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
-	ifne	?L67
+	ifne	?L74
 	dup
 	ldc	"5"
 	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
 	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
-	ifne	?L68
+	ifne	?L75
 	dup
 	ldc	"6"
 	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
 	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
-	ifne	?L69
+	ifne	?L76
+	dup
 	ldc	"7"
 	invokestatic	"System/String" "__FromJavaString" "(Ljava/lang/String;)LSystem/String;"
 	invokestatic	"System/String" "op_Equality" "(LSystem/String;LSystem/String;)Z"
-	ifne	?L70
-	goto	?L66
-?L70:
+	ifne	?L77
+	goto	?L73
+?L77:
+	pop
 	bipush	7
 	istore_2
-	goto	?L66
-?L69:
+	goto	?L78
+?L76:
+	pop
 	bipush	6
 	istore_2
-	goto	?L66
-?L68:
+	goto	?L78
+?L75:
+	pop
 	iconst_5
 	istore_2
-	goto	?L66
-?L67:
+	goto	?L78
+?L74:
+	pop
 	iconst_4
 	istore_2
-	goto	?L66
-?L65:
+	goto	?L78
+?L72:
+	pop
 	iconst_3
 	istore_2
-	goto	?L66
-?L64:
+	goto	?L78
+?L71:
+	pop
 	iconst_2
 	istore_2
-	goto	?L66
-?L63:
+	goto	?L78
+?L70:
+	pop
 	iconst_1
 	istore_2
-	goto	?L66
-?L62:
+	goto	?L78
+?L69:
+	pop
 	iconst_0
 	istore_2
-	goto	?L66
-?L66:
+	goto	?L78
+?L73:
+	pop
+?L78:
 	return
 	.locals 3
 	.maxstack 3
@@ -477,78 +525,86 @@
 	iload_1
 	dup
 	iconst_0
-	if_icmpeq	?L71
+	if_icmpeq	?L79
 	dup
 	iconst_1
-	if_icmpeq	?L72
+	if_icmpeq	?L80
 	dup
 	iconst_2
-	if_icmpeq	?L73
+	if_icmpeq	?L81
+	dup
 	iconst_3
-	if_icmpeq	?L74
-	goto	?L75
-?L71:
-	iconst_0
-	istore_2
-	goto	?L75
-?L72:
-	iconst_1
-	istore_2
-	goto	?L75
-?L73:
-	iconst_2
-	istore_2
-	goto	?L75
-?L74:
-	iconst_3
-	istore_2
-	goto	?L75
-?L75:
-	iload_1
-	tableswitch (
-		?L76,
-		?L77,
-		?L78,
-		?L79,
-		?L80,
-		?L81,
-		?L82,
-		?L83
-	)
-	goto	?L84
-?L76:
-	iconst_0
-	istore_2
-	goto	?L84
-?L77:
-	iconst_1
-	istore_2
-	goto	?L84
-?L78:
-	iconst_2
-	istore_2
-	goto	?L84
+	if_icmpeq	?L82
+	goto	?L83
 ?L79:
-	iconst_3
+	pop
+	iconst_0
 	istore_2
 	goto	?L84
 ?L80:
-	iconst_4
+	pop
+	iconst_1
 	istore_2
 	goto	?L84
 ?L81:
-	iconst_5
+	pop
+	iconst_2
 	istore_2
 	goto	?L84
 ?L82:
-	bipush	6
+	pop
+	iconst_3
 	istore_2
 	goto	?L84
 ?L83:
+	pop
+?L84:
+	iload_1
+	tableswitch (
+		?L85,
+		?L86,
+		?L87,
+		?L88,
+		?L89,
+		?L90,
+		?L91,
+		?L92
+	)
+	goto	?L93
+?L85:
+	iconst_0
+	istore_2
+	goto	?L94
+?L86:
+	iconst_1
+	istore_2
+	goto	?L94
+?L87:
+	iconst_2
+	istore_2
+	goto	?L94
+?L88:
+	iconst_3
+	istore_2
+	goto	?L94
+?L89:
+	iconst_4
+	istore_2
+	goto	?L94
+?L90:
+	iconst_5
+	istore_2
+	goto	?L94
+?L91:
+	bipush	6
+	istore_2
+	goto	?L94
+?L92:
 	bipush	7
 	istore_2
-	goto	?L84
-?L84:
+	goto	?L94
+?L93:
+?L94:
 	return
 	.locals 3
 	.maxstack 3

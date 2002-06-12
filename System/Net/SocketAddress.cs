dacbb015a9eee2479c4e61e902ec68fca555a406
Charlie Carnow <carnow@gmx.net>
2002-06-12 05:29:11 +0000

    Completed all of System.Net.SocketAddress except for GetHashCode()

--- System/Net/SocketAddress.cs
+++ System/Net/SocketAddress.cs
@@ -20,80 +20,204 @@
 
 namespace System.Net
 {
-
+	
 using System;
 using System.Net.Sockets;
 
 public class SocketAddress
 {
 	private byte[] myarray;
+	private AddressFamily family;	
 	
-	//An ipaddress is stored as a long in the same way as in IPAddress
-	//the 2 bytes after the IPAddress are the port
+	public SocketAddress(AddressFamily family) 
+		: SocketAddress(family, 32)
+	{
+	}
 		
-	[TODO]
-	public SocketAddress(AddressFamily family)
-			{
-	
-			}
-	
-	[TODO]
 	public SocketAddress(AddressFamily family, int size)
-			{
-	
-			}
-	
-	[TODO]
-	public override bool Equals(object comparand)
-			{
-				return false;
-			}		
-	
+	{
+		this.family = family;
+		if (size < 2) 
+		{
+				throw new ArgumentOutOfRangeException();
+		}
+		this.myarray = new byte[size];
+	}
+		
+	public override Equals(object comparand) 
+	{
+		if (comparand is SocketAddress)
+		{
+			return ((SocketAddress)comparand).ToString() ==
+			ToString();
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
 	[TODO]
 	public override int GetHashCode()
-			{
-				return 0;
-			}
-	
-	[TODO]
-	public override String ToString()
-			{
-				return null;
-			}
-	
-	[TODO]
-	public AddressFamily Family 
-			{ 
-				get
-				{
-					return 0;
-				}			
-			}
-
-	[TODO]
-	public byte this[int offset]
-			{
-				get
-				{
-					return myarray[offset];				
-				}
-				set
-				{
-					if (offset < 0 || offset > this.Size)
-						throw new ArgumentOutOfRangeException("item");
-					myarray[offset] = value;
-				}
-			}
-	
-	[TODO]
-	public int Size
-			{
-				get
-				{
-					return myarray.Count;
-				}
-			}
+	{
+			return 123;
+	}
 		
-}; //class SocketAddress
+	public override string ToString()
+	{
+		string str;
+		switch(this.family) 
+		{	
+			case AddressFamily.AppleTalk:
+				str += "AppleTalk:";
+				break;
+			case AddressFamily.Atm:
+				str += "Atm:";
+				break;
+			case AddressFamily.Banyan:
+				str += "Banyan:";
+				break;
+			case AddressFamily.Ccitt:
+				str += "Ccit:";
+				break;
+			case AddressFamily.Chaos:
+				str += "Chaos:";
+				break;
+			case AddressFamily.Cluster:
+				str += "Cluster:";
+				break;
+			case AddressFamily.DataKit:
+				str += "DataKit:";
+				break;
+			case AddressFamily.DataLink:
+				str += "DataLink:";
+				break;
+			case AddressFamily.DecNet:
+				str += "DecNet:";
+				break;
+			case AddressFamily.Ecma:
+				str += "Ecma:";
+				break;
+			case AddressFamily.FireFox:
+				str += "FireFox:";
+				break;
+			case AddressFamily.HyperChannel:
+				str += "HyperChannel:";
+				break;
+			case AddressFamily.Ieee1284:
+				str += "Ieee1284:";
+				break;
+			case AddressFamily.ImpLink:
+				str += "ImpLink:";
+				break;		
+			case AddressFamily.InterNetwork:
+				str += "InterNetwork:";
+				break;
+			case AddressFamily.InterNetworkV6:
+				str += "InterNetworkV6:";
+				break;
+			case AddressFamily.Ipx:
+				str += "Ipx:";
+				break;
+			case AddressFamily.Irda:
+				str += "Irda:";
+				break;
+			case AddressFamily.Iso:
+				str += "Iso:";
+				break;
+			case AddressFamily.NS:
+				str += "NS:";
+				break;
+			case AddressFamily.NetBios:
+				str += "NetBios:";
+				break;
+			case AddressFamily.NetworkDesigners:
+				str += "NetworkDesigners:";
+				break;
+			case AddressFamily.Osi:
+				str += "Osi:";
+				break;	
+			case AddressFamily.Pup:
+				str += "Pup:";
+				break;
+			case AddressFamily.Sna:
+				str += "Sna:";
+				break;
+			case AddressFamily.Unix:
+				str += "Unix:";
+				break;
+			case AddressFamily.Unknown:
+				str += "Unknown:";
+				break;
+			case AddressFamiy.Unspecified:
+				str += "Unspecified:";
+				break;
+			case AddressFamily.VoiceView:
+				str += "VoiceView:";
+				break;
+			default:
+				return null;
+		}
+		str += myarray.Count.ToString();
+		str += ':';
+		str += '{';
+		string tmp;
+		foreach(byte x in myarray) 
+		{
+			tmp += x.ToString();
+			tmp += ',';
+		}
+		// Got to get rid of trailing ','	
+		tmp = tmp.Substring(0, tmp.Length-1);
+		str += tmp;
+		str += '}';
+		return str;
+	}
+		
+	public AddressFamily Family
+	{
+		get
+		{
+			return family;
+		}
+	}
+	
+	public byte this[int offset]
+	{
+	 	get
+		{
+			return myarray[offset];				
+		}
+		set
+		{
+			if (offset < 0 || offset > this.Size)
+				throw new ArgumentOutOfRangeException("item");
+				myarray[offset] = value;
+		}
+	}
+	
+	
+	public int Size
+	{
+		get
+		{
+			return myarray.Count;
+		}
+	}	
 
-}; //namespace System.Net
+
+   }
+} // class SocketAddress
+
+
+
+
+
+
+
+
+
+
+
+

04df002b4d956f54e99efde64fec12f028eef54e
Rhys Weatherley <rweather@southern-storm.com.au>
2002-06-18 01:48:43 +0000

    Add the exception class "NotImplementedException".

---
+++ runtime/System/NotImplementedException.cs
@@ -0,0 +1,51 @@
+/*
+ * NotImplementedException.cs - Implementation of the
+ *			"System.NotImplementedException" class.
+ *
+ * Copyright (C) 2001  Southern Storm Software, Pty Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+namespace System
+{
+
+// Strictly speaking, "NotImplementedException" is not defined
+// by the ECMA library specification.  However, other parts of
+// the ECMA specification rely upon its presence.
+
+public class NotImplementedException : SystemException
+{
+
+	// Constructors.
+	public NotImplementedException()
+		: base(_("Exception_NotImplemented")) {}
+	public NotImplementedException(String msg)
+		: base(msg) {}
+	public NotImplementedException(String msg, Exception inner)
+		: base(msg, inner) {}
+
+	// Get the default message to use for this exception type.
+	protected internal override String MessageDefault
+			{
+				get
+				{
+					return _("Exception_NotImplemented");
+				}
+			}
+
+}; // class NotImplementedException
+
+}; // namespace System

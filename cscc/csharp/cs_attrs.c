dc6c7d98ae903ee14da6de5aa6f998f8d9d64f20
Rhys Weatherley <rweather@southern-storm.com.au>
2002-07-19 09:55:44 +0000

    Add quick and dirty support for the "DllImport" attribute.

--- cscc/csharp/cs_attrs.c
+++ cscc/csharp/cs_attrs.c
@@ -24,10 +24,75 @@
 extern	"C" {
 #endif
 
+static void AddDllImport(ILProgramItem *item, const char *dllName)
+{
+	ILModule *module;
+	module = ILModuleRefCreateUnique(ILProgramItem_Image(item), dllName);
+	if(!module)
+	{
+		CCOutOfMemory();
+	}
+	if(ILPInvokeCreate((ILMethod *)item, 0,
+					   IL_META_PINVOKE_CALL_CONV_CDECL, module, 0) == 0)
+	{
+		CCOutOfMemory();
+	}
+	ILMemberSetAttrs((ILMember *)item,
+					 IL_META_METHODDEF_PINVOKE_IMPL,
+					 IL_META_METHODDEF_PINVOKE_IMPL);
+}
+
 void CSProcessAttrs(ILGenInfo *info, ILProgramItem *item,
 					ILNode *attributes, int target)
 {
 	/* TODO */
+
+	/* Quick and dirty hack: recognise the "DllImport" attribute */
+	if(target == CS_ATTR_METHOD && attributes != 0)
+	{
+		ILNode_ListIter iter1;
+		ILNode_ListIter iter2;
+		ILNode_ListIter iter3;
+		ILNode *section, *attr;
+		ILNode *args, *arg;
+		CSSemValue value;
+		ILEvalValue evalValue;
+		char *name;
+		ILNode_ListIter_Init(&iter1, ((ILNode_AttributeTree *)attributes)
+											->sections);
+		while((section = ILNode_ListIter_Next(&iter1)) != 0)
+		{
+			ILNode_ListIter_Init(&iter2, ((ILNode_AttributeSection *)section)
+												->attrs);
+			while((attr = ILNode_ListIter_Next(&iter2)) != 0)
+			{
+				name = ILQualIdentName(((ILNode_Attribute *)attr)->name, 0);
+				if(!strcmp(name, "DllImport") ||
+				   !strcmp(name, "DllImportAttribute"))
+				{
+					args = ((ILNode_AttrArgs *)
+								(((ILNode_Attribute *)attr)->args))
+							->positionalArgs;
+					ILNode_ListIter_Init(&iter3, args);
+					while((arg = ILNode_ListIter_Next(&iter3)) != 0)
+					{
+						if(CSSemExpectValue(arg, info, &arg, &value) &&
+						   ILTypeIsStringClass(value.type))
+						{
+							if(ILNode_EvalConst(arg, info, &evalValue) &&
+							   evalValue.valueType == ILMachineType_String)
+							{
+								AddDllImport(item,
+									ILInternString(evalValue.un.strValue.str,
+												   evalValue.un.strValue.len)
+										.string);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
 void CSProcessAttrsForParam(ILGenInfo *info, ILMethod *method,

cf80c0263cf44d70bec17a8ed8fb62436a1eebab
Rhys Weatherley <rweather@southern-storm.com.au>
2002-08-05 11:33:08 +0000

    Check in a basic directory skeleton for the C test cases.

---
+++ c/decl/type1.c
@@ -0,0 +1,27 @@
+/*
+ * type1.cs - Test simple type declarations.
+ *
+ * Copyright (C) 2002  Southern Storm Software, Pty Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+struct x
+{
+	int y;
+};
+
+/* We must reference the types to cause them to be output to the .il file */
+void f(struct x) {}

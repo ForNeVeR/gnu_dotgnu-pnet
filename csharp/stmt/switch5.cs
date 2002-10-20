6a5713fa540393c0f4ed91a23e2a5514d6b116fe
Gopal V <gopalv82@yahoo.com>
2002-10-20 20:50:12 +0000

    tests for `goto case' and `goto default'

---
+++ csharp/stmt/switch5.cs
@@ -0,0 +1,23 @@
+public class SwitchBlade
+{
+	public int SwitchMan(int last)
+	{
+		goto default;
+		switch(last)
+		{
+			case 42:
+				try
+				{
+				}
+				finally
+				{
+				goto case 84;
+				}
+				return 1;
+			case 84:
+				return 2;
+		}
+		goto case 12; 
+		return 0;
+	}
+}

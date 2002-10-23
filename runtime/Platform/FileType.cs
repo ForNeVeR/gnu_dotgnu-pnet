6feb95a82a21a99bd9b312743723c9546e2f114e
Gopal V <gopalv82@yahoo.com>
2002-10-23 12:54:34 +0000

    values for the FileType enum

--- runtime/Platform/FileType.cs
+++ runtime/Platform/FileType.cs
@@ -2,13 +2,14 @@ namespace Platform
 {
 	internal enum FileType
 	{
-		directory,
-		characterSpecial,
-		blockSpecial,
-		regularFile,
-		fifoSpecial,
-		symbolicLink,
-		socket
+		unknown=0,
+		fifoSpecial=1,
+		characterSpecial=2,
+		directory=4,
+		blockSpecial=6,
+		regularFile=8,
+		symbolicLink=10,
+		socket=12
 	}
 
 } //namespace Platform

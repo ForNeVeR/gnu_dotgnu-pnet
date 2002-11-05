aa970f151932bc937bc4a267897cfece2a2e6bd2
Rhys Weatherley <rweather@southern-storm.com.au>
2002-11-05 00:38:02 +0000

    Clean up the list of semantic kinds.

--- cscc/csharp/cs_semvalue.h
+++ cscc/csharp/cs_semvalue.h
@@ -61,9 +61,8 @@ extern CSSemValue CSSemValueError;
 #define	CS_SEMKIND_INDEXER_GROUP	(1<<12)
 #define	CS_SEMKIND_CONSTANT			(1<<13)
 #define	CS_SEMKIND_DYN_CONSTANT		(1<<14)
-/*#define	CS_SEMKIND_TYPE_NODE		(1<<15)*/
-#define	CS_SEMKIND_ERROR			(1<<16)
-#define	CS_SEMKIND_BASE				(1<<17)
+#define	CS_SEMKIND_ERROR			(1<<15)
+#define	CS_SEMKIND_BASE				(1<<16)
 
 /*
  * Set a semantic value to "void".

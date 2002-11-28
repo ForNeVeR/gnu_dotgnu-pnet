7f5395be57f1fe72633e5762aff6d3e8ba510948
Rhys Weatherley <rweather@southern-storm.com.au>
2002-11-28 06:22:42 +0000

    The UTF8 object should be created with preambles by default.

--- runtime/System/Text/Encoding.cs
+++ runtime/System/Text/Encoding.cs
@@ -726,7 +726,7 @@ public abstract class Encoding
 					{
 						if(utf8Encoding == null)
 						{
-							utf8Encoding = new UTF8Encoding();
+							utf8Encoding = new UTF8Encoding(true);
 						}
 						return utf8Encoding;
 					}

ddb7924df0b4052051937388e47d0ca7bbda8e28
Rhys Weatherley <rweather@southern-storm.com.au>
2003-01-12 06:47:01 +0000

    Implement the debug assert routines using simple "report to stderr" behaviour.

--- System/Diagnostics/Debug.cs
+++ System/Diagnostics/Debug.cs
@@ -23,208 +23,305 @@
 
 namespace System.Diagnostics
 {
-	public sealed class Debug
-	{
-		[TODO]
-		public static void Assert(bool condition)
-		{
-		/*TODO*/
-		}
 
-		[TODO]
-		public static void Assert(bool condition, System.String message)
-		{
-		/*TODO*/
-		}
+using System;
 
-		[TODO]
-		public static void Assert(bool condition, System.String message, System.String detailMessage)
-		{
-		/*TODO*/
-		}
+#define	DEBUG
 
-		[TODO]
-		public static void Close()
-		{
-		/*TODO*/
-		}
+public sealed class Debug
+{
+	// Internal state.
+	private static bool autoFlush = true;
+	private static int indentLevel = 0;
+	private static int indentSize = 4;
 
-		[TODO]
-		public static void Fail(System.String message)
-		{
-		/*TODO*/
-		}
+	// Cannot construct instances of this class.
+	private Debug() {}
 
-		[TODO]
-		public static void Fail(System.String message, System.String detailMessage)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		public static void Flush()
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		public static void Indent()
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		public static void Unindent()
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void Write(System.Object value)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void Write(System.String message)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void Write(System.Object value, System.String category)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void Write(System.String message, System.String category)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteIf(bool condition, System.Object value)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteIf(bool condition, System.String message)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteIf(bool condition, System.Object value, System.String category)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteIf(bool condition, System.String message, System.String category)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteLine(System.Object value)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteLine(System.String message)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteLine(System.Object value, System.String category)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteLine(System.String message, System.String category)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteLineIf(bool condition, System.Object value)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteLineIf(bool condition, System.String message)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteLineIf(bool condition, System.Object value, System.String category)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		[Conditional("DEBUG")]
-		public static void WriteLineIf(bool condition, System.String message, System.String category)
-		{
-		/*TODO*/
-		}
-
-		[TODO]
-		public static bool AutoFlush 
-		{ 
-			get
+	// Assert a condition, exiting the application if it is false.
+	[Conditional("DEBUG")]
+	public static void Assert(bool condition)
 			{
-				return false;
-			} 
-			set
-			{
-			} 
-		}
-
-		[TODO]
-		public static int IndentLevel 
-		{ 
-			get
-			{
-				return 0;
+				if(!condition)
+				{
+					Fail(String.Empty);
+				}
 			}
-			set
+	[Conditional("DEBUG")]
+	public static void Assert(bool condition, String message)
 			{
+				if(!condition)
+				{
+					Fail(message);
+				}
 			}
-		}
+	[Conditional("DEBUG")]
+	public static void Assert
+				(bool condition, String message, String detailMessage)
+			{
+				if(!condition)
+				{
+					Fail(message, detailMessage);
+				}
+			}
 
-		[TODO]
-		public static int IndentSize 
-		{
-			get
+	// Close the trace output stream.
+	[Conditional("DEBUG")]
+	public static void Close()
 			{
-				return 4;
+				// Nothing to do here.
 			}
-			set
+
+	// Report a debugging failure and exit the application.
+	[Conditional("DEBUG")]
+	public static void Fail(String message)
 			{
+				Fail(message, String.Empty);
 			}
-		}
-	}
-}//namespace
+	[Conditional("DEBUG")]
+	public static void Fail(String message, String detailMessage)
+			{
+				// Print the failure message details.
+				if(message != null && message != String.Empty)
+				{
+					WriteLine(message);
+				}
+				if(detailMessage != null && detailMessage != String.Empty)
+				{
+					WriteLine(detailMessage);
+				}
+
+				// Print the stack trace for the failure.
+				Write((new StackTrace()).ToString());
+				Flush();
+
+				// Abort the application.
+				Environment.Exit(1);
+			}
+
+	// Flush the trace output stream.
+	[Conditional("DEBUG")]
+	public static void Flush()
+			{
+				Console.Error.Flush();
+			}
+
+	// Go in one indent level within the trace output stream.
+	[Conditional("DEBUG")]
+	public static void Indent()
+			{
+				IndentLevel += 1;
+			}
+
+	// Go out one indent level within the trace output stream.
+	[Conditional("DEBUG")]
+	public static void Unindent()
+			{
+				IndentLevel -= 1;
+			}
+
+	// Write data to the trace output stream.
+	[Conditional("DEBUG")]
+	public static void Write(Object value)
+			{
+				if(value != null)
+				{
+					Write(value.ToString());
+				}
+			}
+	[TODO]
+	[Conditional("DEBUG")]
+	public static void Write(String message)
+			{
+				Console.Error.Write(message);
+			}
+	[Conditional("DEBUG")]
+	public static void Write(Object value, String category)
+			{
+				if(category == null)
+				{
+					Write(value);
+				}
+				else
+				{
+					Write((value != null ? value.ToString() : String.Empty),
+						  category);
+				}
+			}
+	[Conditional("DEBUG")]
+	public static void Write(String message, String category)
+			{
+				if(category == null)
+				{
+					Write(message);
+				}
+				else
+				{
+					Write(category + ": " +
+						  (message != null ? message : String.Empty));
+				}
+			}
+
+	// Write a debugging message if a condition is true.
+	[Conditional("DEBUG")]
+	public static void WriteIf(bool condition, Object value)
+			{
+				if(condition)
+				{
+					Write(value);
+				}
+			}
+	[Conditional("DEBUG")]
+	public static void WriteIf(bool condition, String message)
+			{
+				if(condition)
+				{
+					Write(message);
+				}
+			}
+	[Conditional("DEBUG")]
+	public static void WriteIf(bool condition, Object value, String category)
+			{
+				if(condition)
+				{
+					Write(value, category);
+				}
+			}
+	[Conditional("DEBUG")]
+	public static void WriteIf(bool condition, String message, String category)
+			{
+				if(condition)
+				{
+					Write(message, category);
+				}
+			}
+
+	// Write a message to the debug output stream followed by newline.
+	[Conditional("DEBUG")]
+	public static void WriteLine(Object value)
+			{
+				if(value != null)
+				{
+					WriteLine(value.ToString());
+				}
+			}
+	[TODO]
+	[Conditional("DEBUG")]
+	public static void WriteLine(String message)
+			{
+				Console.Error.WriteLine(message);
+			}
+	[Conditional("DEBUG")]
+	public static void WriteLine(Object value, String category)
+			{
+				if(category == null)
+				{
+					WriteLine(value);
+				}
+				else
+				{
+					WriteLine((value != null ?
+									value.ToString() : String.Empty),
+						       category);
+				}
+			}
+	[Conditional("DEBUG")]
+	public static void WriteLine(String message, String category)
+			{
+				if(category == null)
+				{
+					WriteLine(message);
+				}
+				else
+				{
+					WriteLine(category + ": " +
+						      (message != null ? message : String.Empty));
+				}
+			}
+
+	// Write a debugging message if a condition is true, followed by newline.
+	[Conditional("DEBUG")]
+	public static void WriteLineIf(bool condition, Object value)
+			{
+				if(condition)
+				{
+					WriteLine(value);
+				}
+			}
+	[Conditional("DEBUG")]
+	public static void WriteLineIf(bool condition, String message)
+			{
+				if(condition)
+				{
+					WriteLine(message);
+				}
+			}
+	[Conditional("DEBUG")]
+	public static void WriteLineIf
+					(bool condition, Object value, String category)
+			{
+				if(condition)
+				{
+					WriteLine(value, category);
+				}
+			}
+	[Conditional("DEBUG")]
+	public static void WriteLineIf
+					(bool condition, String message, String category)
+			{
+				if(condition)
+				{
+					WriteLine(message, category);
+				}
+			}
+
+	// Get or set the message auto-flush state.
+	public static bool AutoFlush 
+			{ 
+				get
+				{
+					return autoFlush;
+				} 
+				set
+				{
+					autoFlush = value;
+				} 
+			}
+
+	// Change the indent level for output messages.
+	public static int IndentLevel 
+			{ 
+				get
+				{
+					return indentLevel;
+				}
+				set
+				{
+					if(value < 0)
+					{
+						value = 0;
+					}
+					indentLevel = value;
+				}
+			}
+
+	// Set the size of output message indenting.
+	public static int IndentSize 
+			{
+				get
+				{
+					return indentSize;
+				}
+				set
+				{
+					if(value < 0)
+					{
+						value = 0;
+					}
+					indentSize = value;
+				}
+			}
+
+}; // class Debug
+
+}; // namespace System.Diagnostics

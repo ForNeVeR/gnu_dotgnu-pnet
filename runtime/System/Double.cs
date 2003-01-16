c6ca8a916d2b5b2479f61156f3b1d6e928e950be
Gopal V <gopalv82@yahoo.com>
2003-01-16 15:33:59 +0000

    check out last commit

--- runtime/System/Double.cs
+++ runtime/System/Double.cs
@@ -105,7 +105,7 @@ public struct Double : IComparable, IFormattable
 			}
 	public String ToString(String format, IFormatProvider provider)
 			{
-				if (format == null) format = "G2";
+				if (format == null) format = "G";
 				return
 					Formatter.CreateFormatter(format).Format(this, provider);
 			}

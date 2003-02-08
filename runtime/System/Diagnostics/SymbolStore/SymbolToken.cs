4cb83d3ccad2b4de00a2230b6922249fc327ed5a
Gopal V <gopalv82@yahoo.com>
2003-02-08 05:58:21 +0000

    add non-ECMA System.Diagnostics.SymbolStore classes

---
+++ runtime/System/Diagnostics/SymbolStore/SymbolToken.cs
@@ -0,0 +1,54 @@
+/*
+ * SymbolToken.cs - Implementation of 
+ *				"System.Diagnostics.SymbolStore.SymbolToken" struct.
+ *
+ * Copyright (C) 2003  Southern Storm Software, Pty Ltd.
+ * 
+ * Contributed by Gopal.V
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+using System;
+
+namespace System.Diagnostics.SymbolStore
+{
+	public struct SymbolToken
+	{
+		[TODO]
+		public SymbolToken(int val)
+		{
+			 throw new NotImplementedException(".ctor");
+		}
+
+		[TODO]
+		public int GetToken()
+		{
+			 throw new NotImplementedException("GetToken");
+		}
+
+		[TODO]
+		public override bool Equals(Object obj)
+		{
+			 throw new NotImplementedException("Equals");
+		}
+
+		[TODO]
+		public override int GetHashCode()
+		{
+			 throw new NotImplementedException("GetHashCode");
+		}
+	}
+}//namespace

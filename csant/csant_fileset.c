9f31c8b1569cb86667347d052a41b35f0edca9fd
Rhys Weatherley <rweather@southern-storm.com.au>
2003-02-23 01:24:42 +0000

    Rearrange the code for CSAntFileSetLoad so that ".." directory includes are handled correctly.

--- csant/csant_fileset.c
+++ csant/csant_fileset.c
@@ -50,6 +50,7 @@ typedef struct _tagCSAntDirStack CSAntDirStack;
 struct _tagCSAntDirStack
 {
 	char          *dirName;
+	const char    *regex;
 	CSAntDirStack *next;
 
 };
@@ -95,169 +96,139 @@ static int CheckIf(CSAntTask *node)
 }
 
 /*
- * Build a regular expression from a set of include or exclude names.
+ * Build a regular expression from a wildcard argument specification.
  * Returns non-zero if recursion into sub-directories is specified.
  */
 typedef regex_t RegexState;
-static int BuildIncludeRegex(CSAntTask *node, const char *name,
-							 RegexState *state)
+static int BuildIncludeRegex(const char *arg, RegexState *state)
 {
-	CSAntTask *subNode;
-	const char *arg;
 	char *regex = 0;
 	int regexLen = 0;
 	int regexMax = 0;
-	int haveCase = 0;
 	int recursive = 0;
 	char ch;
 
 	/* Build the regular expression */
-	subNode = node->taskChildren;
-	while(subNode != 0)
+	REGEX_CHAR('^');
+	while(*arg != 0)
 	{
-		if(!CheckIf(subNode))
+		ch = *arg++;
+		if(ch == '*' && *arg == '*')
 		{
-			subNode=subNode->next;
-			continue;
-		}
-		if(!strcmp(subNode->name, name) &&
-		   (arg = CSAntTaskParam(subNode, "name")) != 0)
-		{
-			if(haveCase)
+			/* Recursive regular expression definition */
+			recursive = 1;
+			if(arg[1] == '/' || arg[1] == '\\')
 			{
+				REGEX_CHAR('(');
+				REGEX_CHAR('.');
+				REGEX_CHAR('*');
+			#ifdef IL_WIN32_NATIVE
+				REGEX_CHAR('\\');
+				REGEX_CHAR('\\');
+			#else
+				REGEX_CHAR('/');
+			#endif
 				REGEX_CHAR('|');
+				REGEX_CHAR('(');
+				REGEX_CHAR(')');
+				REGEX_CHAR(')');
+				arg += 2;
 			}
 			else
 			{
-				haveCase = 1;
+				REGEX_CHAR('.');
+				REGEX_CHAR('*');
+				++arg;
 			}
-			REGEX_CHAR('^');
-			while(*arg != 0)
-			{
-				ch = *arg++;
-				if(ch == '*' && *arg == '*')
-				{
-					/* Recursive regular expression definition */
-					recursive = 1;
-					if(arg[1] == '/' || arg[1] == '\\')
-					{
-						REGEX_CHAR('(');
-						REGEX_CHAR('.');
-						REGEX_CHAR('*');
-					#ifdef IL_WIN32_NATIVE
-						REGEX_CHAR('\\');
-						REGEX_CHAR('\\');
-					#else
-						REGEX_CHAR('/');
-					#endif
-						REGEX_CHAR('|');
-						REGEX_CHAR('(');
-						REGEX_CHAR(')');
-						REGEX_CHAR(')');
-						arg += 2;
-					}
-					else
-					{
-						REGEX_CHAR('.');
-						REGEX_CHAR('*');
-						++arg;
-					}
-				}
-				else if(ch == '*')
-				{
-					/* Match anything that doesn't involve a separator */
-					REGEX_CHAR('[');
-					REGEX_CHAR('^');
-				#ifdef IL_WIN32_NATIVE
-					REGEX_CHAR('\\');
-					REGEX_CHAR('\\');
-				#else
-					REGEX_CHAR('/');
-				#endif
-					REGEX_CHAR(']');
-					REGEX_CHAR('*');
-				}
-				else if(ch == '?')
-				{
-					/* Match a single character */
-					REGEX_CHAR('.');
-				}
-				else if(ch == '/' || ch == '\\')
-				{
-					/* Match a directory separator */
-					recursive = 1;
-				#ifdef IL_WIN32_NATIVE
-					REGEX_CHAR('\\');
-					REGEX_CHAR('\\');
-				#else
-					REGEX_CHAR('/');
-				#endif
-				}
-				else if(ch == '[')
-				{
-					/* Match a set of characters */
-					REGEX_CHAR('[');
-					while(*arg != 0 && *arg != ']')
-					{
-						REGEX_CHAR(*arg);
-						++arg;
-					}
-					REGEX_CHAR(']');
-					if(*arg != '\0')
-					{
-						++arg;
-					}
-				}
-				else if(ch == '.' || ch == '^' || ch == '$' ||
-						ch == ']' || ch == '\\' || ch == '(' || ch == ')')
-				{
-					/* Match a special character */
-					REGEX_CHAR('\\');
-					REGEX_CHAR(ch);
-				}
-				else
-				{
-					/* Match an ordinary character */
-				#ifdef _WIN32
-					if(ch >= 'A' && ch <= 'Z')
-					{
-						REGEX_CHAR('[');
-						REGEX_CHAR(ch);
-						REGEX_CHAR(ch - 'A' + 'a');
-						REGEX_CHAR(']');
-					}
-					else if(ch >= 'a' && ch <= 'z')
-					{
-						REGEX_CHAR('[');
-						REGEX_CHAR(ch - 'a' + 'A');
-						REGEX_CHAR(ch);
-						REGEX_CHAR(']');
-					}
-					else
-					{
-						REGEX_CHAR(ch);
-					}
-				#else
-					REGEX_CHAR(ch);
-				#endif
-				}
-			}
-			REGEX_CHAR('$');
 		}
-		subNode = subNode->next;
-	}
-
-	/* If there are no cases, then construct a regex that is
-	   guaranteed never to match any filenames */
-	if(!haveCase)
-	{
-		REGEX_CHAR('^');
-		REGEX_CHAR('(');
-		REGEX_CHAR(')');
-		REGEX_CHAR('$');
+		else if(ch == '*')
+		{
+			/* Match anything that doesn't involve a separator */
+			REGEX_CHAR('[');
+			REGEX_CHAR('^');
+		#ifdef IL_WIN32_NATIVE
+			REGEX_CHAR('\\');
+			REGEX_CHAR('\\');
+		#else
+			REGEX_CHAR('/');
+		#endif
+			REGEX_CHAR(']');
+			REGEX_CHAR('*');
+		}
+		else if(ch == '?')
+		{
+			/* Match a single character */
+			REGEX_CHAR('.');
+		}
+		else if(ch == '/' || ch == '\\')
+		{
+			/* Match a directory separator */
+			recursive = 1;
+		#ifdef IL_WIN32_NATIVE
+			REGEX_CHAR('\\');
+			REGEX_CHAR('\\');
+		#else
+			REGEX_CHAR('/');
+		#endif
+		}
+		else if(ch == '[')
+		{
+			/* Match a set of characters */
+			REGEX_CHAR('[');
+			while(*arg != 0 && *arg != ']')
+			{
+				REGEX_CHAR(*arg);
+				++arg;
+			}
+			REGEX_CHAR(']');
+			if(*arg != '\0')
+			{
+				++arg;
+			}
+		}
+		else if(ch == '.' || ch == '^' || ch == '$' ||
+				ch == ']' || ch == '\\' || ch == '(' || ch == ')')
+		{
+			/* Match a special character */
+			REGEX_CHAR('\\');
+			REGEX_CHAR(ch);
+		}
+		else
+		{
+			/* Match an ordinary character */
+		#ifdef _WIN32
+			if(ch >= 'A' && ch <= 'Z')
+			{
+				REGEX_CHAR('[');
+				REGEX_CHAR(ch);
+				REGEX_CHAR(ch - 'A' + 'a');
+				REGEX_CHAR(']');
+			}
+			else if(ch >= 'a' && ch <= 'z')
+			{
+				REGEX_CHAR('[');
+				REGEX_CHAR(ch - 'a' + 'A');
+				REGEX_CHAR(ch);
+				REGEX_CHAR(']');
+			}
+			else
+			{
+				REGEX_CHAR(ch);
+			}
+		#else
+			REGEX_CHAR(ch);
+		#endif
+		}
 	}
 
 	/* Terminate the regular expression */
+	if(regexLen == 1)
+	{
+		/* The regular expression was empty, so match nothing */
+		REGEX_CHAR('(');
+		REGEX_CHAR(')');
+	}
+	REGEX_CHAR('$');
 	REGEX_CHAR('\0');
 
 	/* Compile the register expression and exit */
@@ -298,6 +269,28 @@ static void AddToFileSet(CSAntFileSet *fileset, char *pathname)
 }
 
 /*
+ * Remove a pathname from a file set.
+ */
+static void RemoveFromFileSet(CSAntFileSet *fileset, char *pathname)
+{
+	unsigned long posn;
+	for(posn = 0; posn < fileset->numFiles; ++posn)
+	{
+		if(!strcmp(fileset->files[posn], pathname))
+		{
+			ILFree(fileset->files[posn]);
+			--(fileset->numFiles);
+			while(posn < fileset->numFiles)
+			{
+				fileset->files[posn] = fileset->files[posn + 1];
+				++posn;
+			}
+			return;
+		}
+	}
+}
+
+/*
  * Determine if a pathname corresponds to a directory.
  */
 static int IsDirectory(const char *pathname)
@@ -317,67 +310,144 @@ static int IsDirectory(const char *pathname)
 #endif
 }
 
-CSAntFileSet *CSAntFileSetLoad(CSAntTask *task, const char *name,
-							   const char *configBaseDir)
+/*
+ * Add directory and regex information to a directory stack for
+ * a particular "includes" specification.
+ */
+static CSAntDirStack *DirStackAddInclude
+			(CSAntDirStack *stack, CSAntFileSet *fileset,
+			 CSAntTask *include, const char *baseDir,
+			 int addToFileSet)
 {
-	CSAntFileSet *fileset;
-	CSAntTask *node;
 	const char *arg;
-	const char *arg2;
-	char *baseDir;
-	char *currentDir;
+	int posn, start;
 	char *pathname;
-	char *tempName;
-	CSAntDirStack *stack;
-	CSAntDirStack *nextStack;
-	CSAntDir *dir;
-	const char *filename;
-	int recursive;
-	RegexState state;
-	unsigned long posn;
-	unsigned long outposn;
+	char *newBase;
+	CSAntDirStack *item;
 
-	/* Find the sub-node */
-	node = task->taskChildren;
-	while(node != 0 && strcmp(node->name, name) != 0)
+	/* Get the name argument */
+	arg = CSAntTaskParam(include, "name");
+	if(!arg)
 	{
-		node = node->next;
-	}
-	if(!node)
-	{
-		return 0;
+		return stack;
 	}
 
-	/* Construct the starting base directory and add it to the stack */
-	arg = CSAntTaskParam(node, "basedir");
-	if(arg)
+	/* Extract the leading directory information before the wildcards
+	   to find the new base to start at */
+	posn = 0;
+	start = 0;
+	while(arg[posn] != '\0')
 	{
-		baseDir = CSAntDirCombine(configBaseDir, arg);
+		while(arg[posn] != '/' && arg[posn] != '\\' && arg[posn] != '\0')
+		{
+			if(arg[posn] == '*' || arg[posn] == '?' || arg[posn] == '[')
+			{
+				break;
+			}
+			++posn;
+		}
+		if(arg[posn] == '*' || arg[posn] == '?' || arg[posn] == '[')
+		{
+			break;
+		}
+		else if(arg[posn] != '\0')
+		{
+			++posn;
+		}
+		start = posn;
+	}
+	if(arg[start] == '\0')
+	{
+		/* This is a literal pathname with no wildcards */
+		pathname = CSAntDirCombine(baseDir, arg);
+		if(addToFileSet)
+		{
+			AddToFileSet(fileset, pathname);
+		}
+		else
+		{
+			RemoveFromFileSet(fileset, pathname);
+			ILFree(pathname);
+		}
+		return stack;
+	}
+
+	/* Build the new base directory */
+	if(start != 0)
+	{
+		pathname = ILDupNString(arg, start - 1);
+		if(!pathname)
+		{
+			CSAntOutOfMemory();
+		}
+		newBase = CSAntDirCombine(baseDir, pathname);
+		ILFree(pathname);
+		arg += start;
 	}
 	else
 	{
-		baseDir = CSAntDirCombine(configBaseDir, 0);
+		newBase = ILDupString(baseDir);
+		if(!newBase)
+		{
+			CSAntOutOfMemory();
+		}
 	}
-	stack = (CSAntDirStack *)ILMalloc(sizeof(CSAntDirStack));
-	if(!stack)
+
+	/* Add the directory and regex to the stack */
+	item = (CSAntDirStack *)ILMalloc(sizeof(CSAntDirStack));
+	if(!item)
 	{
 		CSAntOutOfMemory();
 	}
-	stack->dirName = baseDir;
-	stack->next = 0;
+	item->dirName = newBase;
+	item->regex = arg;
+	item->next = stack;
+	return item;
+}
 
-	/* Construct the file set */
-	fileset = (CSAntFileSet *)ILMalloc(sizeof(CSAntFileSet));
-	if(!fileset)
+/*
+ * Add a group of includes to a directory stack.
+ */
+static CSAntDirStack *DirStackAddIncludes(CSAntFileSet *fileset,
+										  CSAntTask *task,
+										  const char *name,
+										  const char *baseDir,
+										  int addToFileSet)
+{
+	CSAntDirStack *stack = 0;
+	CSAntTask *subNode;
+	subNode = task->taskChildren;
+	while(subNode != 0)
 	{
-		CSAntOutOfMemory();
+		if(CheckIf(subNode))
+		{
+			if(!strcmp(subNode->name, name))
+			{
+				stack = DirStackAddInclude
+					(stack, fileset, subNode, baseDir, addToFileSet);
+			}
+		}
+		subNode = subNode->next;
 	}
-	fileset->numFiles = 0;
-	fileset->maxFiles = 0;
-	fileset->files = 0;
+	return stack;
+}
 
-	/* Compile the "includes" regular expression */
-	recursive = BuildIncludeRegex(node, "includes", &state);
+/*
+ * Scan a directory start to get files to add to or remove from a fileset.
+ */
+static void ProcessDirStack(CSAntFileSet *fileset, CSAntDirStack *stack,
+							int addToFileSet)
+{
+	RegexState state;
+	int haveRegex = 0;
+	char *currentDir;
+	const char *currentRegex;
+	CSAntDirStack *nextStack;
+	int recursive = 0;
+	CSAntDir *dir;
+	const char *filename;
+	char *pathname;
+	char *tempName;
 
 	/* Pop items from the directory stack and scan them until
 	   all matches have been processed */
@@ -385,9 +455,21 @@ CSAntFileSet *CSAntFileSetLoad(CSAntTask *task, const char *name,
 	{
 		/* Pop the top-most stack item */
 		currentDir = stack->dirName;
+		currentRegex = stack->regex;
 		nextStack = stack->next;
 		ILFree(stack);
 		stack = nextStack;
+
+		/* Create a new regular expression block if necessary */
+		if(currentRegex)
+		{
+			if(haveRegex)
+			{
+				IL_regfree(&state);
+			}
+			recursive = BuildIncludeRegex(currentRegex, &state);
+			haveRegex = 1;
+		}
 
 		/* Scan the directory for inclusions and exclusions */
 		dir = CSAntDirOpen(currentDir, (const char *)0);
@@ -407,15 +489,23 @@ CSAntFileSet *CSAntFileSetLoad(CSAntTask *task, const char *name,
 				/* Different processing is required for directories and files */
 				if(recursive && IsDirectory(pathname))
 				{
-					/* Add to the fileset if we have a match */
-					if(MatchInclude(pathname + strlen(baseDir) + 1, &state))
+					/* Add/Remove to the fileset if we have a match */
+					if(MatchInclude(pathname + strlen(currentDir) + 1, &state))
 					{
 						tempName = ILDupString(pathname);
 						if(!tempName)
 						{
 							CSAntOutOfMemory();
 						}
-						AddToFileSet(fileset, tempName);
+						if(addToFileSet)
+						{
+							AddToFileSet(fileset, tempName);
+						}
+						else
+						{
+							RemoveFromFileSet(fileset, tempName);
+							ILFree(tempName);
+						}
 					}
 
 					/* Add the directory to the stack for later processing */
@@ -426,15 +516,24 @@ CSAntFileSet *CSAntFileSetLoad(CSAntTask *task, const char *name,
 						CSAntOutOfMemory();
 					}
 					nextStack->dirName = pathname;
+					nextStack->regex = 0;
 					nextStack->next = stack;
 					stack = nextStack;
 				}
 				else
 				{
-					/* Add the pathname to the file set if it matches */
-					if(MatchInclude(pathname + strlen(baseDir) + 1, &state))
+					/* Add/Remove the pathname to the file set if it matches */
+					if(MatchInclude(pathname + strlen(currentDir) + 1, &state))
 					{
-						AddToFileSet(fileset, pathname);
+						if(addToFileSet)
+						{
+							AddToFileSet(fileset, pathname);
+						}
+						else
+						{
+							RemoveFromFileSet(fileset, pathname);
+							ILFree(pathname);
+						}
 					}
 					else
 					{
@@ -446,32 +545,70 @@ CSAntFileSet *CSAntFileSetLoad(CSAntTask *task, const char *name,
 		}
 
 		/* Free the current directory name, which we no longer require */
-		if(currentDir != baseDir)
-		{
-			ILFree(currentDir);
-		}
+		ILFree(currentDir);
 	}
 
 	/* Free the regular expression state */
-	IL_regfree(&state);
-
-	/* Construct a new regular expression for the "excludes" list */
-	BuildIncludeRegex(node, "excludes", &state);
-
-	/* Remove pathnames from the fileset that match the regex */
-	outposn = 0;
-	for(posn = 0; posn < fileset->numFiles; ++posn)
+	if(haveRegex)
 	{
-		if(!MatchInclude(fileset->files[posn] + strlen(baseDir) + 1, &state))
-		{
-			fileset->files[outposn++] = fileset->files[posn];
-		}
-		else
-		{
-			ILFree(fileset->files[posn]);
-		}
+		IL_regfree(&state);
 	}
-	fileset->numFiles = outposn;
+}
+
+CSAntFileSet *CSAntFileSetLoad(CSAntTask *task, const char *name,
+							   const char *configBaseDir)
+{
+	CSAntFileSet *fileset;
+	CSAntTask *node;
+	const char *arg;
+	const char *arg2;
+	char *baseDir;
+	char *pathname;
+	CSAntDirStack *stack;
+
+	/* Find the sub-node */
+	node = task->taskChildren;
+	while(node != 0 && strcmp(node->name, name) != 0)
+	{
+		node = node->next;
+	}
+	if(!node)
+	{
+		return 0;
+	}
+
+	/* Construct the starting base directory */
+	arg = CSAntTaskParam(node, "basedir");
+	if(arg)
+	{
+		baseDir = CSAntDirCombine(configBaseDir, arg);
+	}
+	else
+	{
+		baseDir = CSAntDirCombine(configBaseDir, 0);
+	}
+
+	/* Construct the file set */
+	fileset = (CSAntFileSet *)ILMalloc(sizeof(CSAntFileSet));
+	if(!fileset)
+	{
+		CSAntOutOfMemory();
+	}
+	fileset->numFiles = 0;
+	fileset->maxFiles = 0;
+	fileset->files = 0;
+
+	/* Build the directory stack for the "includes" specification */
+	stack = DirStackAddIncludes(fileset, node, "includes", baseDir, 1);
+
+	/* Process the directory stack for the "includes" specification */
+	ProcessDirStack(fileset, stack, 1);
+
+	/* Build the directory stack for the "excludes" specification */
+	stack = DirStackAddIncludes(fileset, node, "excludes", baseDir, 0);
+
+	/* Process the directory stack for the "excludes" specification */
+	ProcessDirStack(fileset, stack, 0);
 
 	/* Add simple files that are named using the <file> tag */
 	node = node->taskChildren;
@@ -499,9 +636,6 @@ CSAntFileSet *CSAntFileSetLoad(CSAntTask *task, const char *name,
 
 	/* We don't need the base directory any more */
 	ILFree(baseDir);
-
-	/* Free the regular expression state */
-	IL_regfree(&state);
 
 	/* Done */
 	return fileset;

8815838b3764a99fc98d882fa5cf9f35fed46082
Rhys Weatherley <rweather@southern-storm.com.au>
2003-03-29 06:44:43 +0000

    Misspelt enum member.

--- runtime/System/Security/Permissions/SecurityAction.cs
+++ runtime/System/Security/Permissions/SecurityAction.cs
@@ -25,15 +25,15 @@ namespace System.Security.Permissions
 public enum SecurityAction
 {
 
-	Demand           = 2,
-	Assert           = 3,
-	Deny             = 4,
-	PermitOnly       = 5,
-	LinkDemand       = 6,
-	InheritaneDemand = 7,
-	RequestMinimum   = 8,
-	RequestOptional  = 9,
-	RequestRefuse    = 10
+	Demand            = 2,
+	Assert            = 3,
+	Deny              = 4,
+	PermitOnly        = 5,
+	LinkDemand        = 6,
+	InheritanceDemand = 7,
+	RequestMinimum    = 8,
+	RequestOptional   = 9,
+	RequestRefuse     = 10
 
 }; // enum SecurityAction
 

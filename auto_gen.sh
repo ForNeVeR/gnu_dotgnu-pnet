fd5eb67d61fdd9c71a2e305d7f3201de7116d75f
Rhys Weatherley <rweather@southern-storm.com.au>
2003-06-08 09:16:10 +0000

    Back out previous change to auto_gen.sh as it causes other problems.

--- auto_gen.sh
+++ auto_gen.sh
@@ -19,7 +19,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 # Run aclocal to update the macros.
-aclocal -I .
+aclocal
 
 # Get extra options to use depending upon the automake version.
 AM_VERSION=`automake --version`

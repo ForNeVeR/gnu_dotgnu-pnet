56bd4e63f6e4a286bc6af6179e10c394f995b38f
Richard Baumann <biochem333@nyc.rr.com>
2003-07-28 18:38:04 +0000

    Commit winforms StatusBar fix for Simon.

--- System.Windows.Forms/StatusBar.cs
+++ System.Windows.Forms/StatusBar.cs
@@ -267,7 +267,167 @@ namespace System.Windows.Forms
 			base.WndProc(ref m);
 		}
 		#endif // !CONFIG_COMPACT_FORMS
-		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		public class StatusBarPanelCollection : IList, ICollection, IEnumerable	
+		{
+			private StatusBar owner;
+			private ArrayList list;
+
+			public StatusBarPanelCollection(StatusBar owner)
+			{
+				this.owner = owner;
+				list = new ArrayList();
+			}
+
+			// Implement the ICollection interface.
+			void ICollection.CopyTo(Array array, int index)
+			{
+				List.CopyTo(array, index);
+			}
+
+			public virtual int Count
+			{
+				get { return List.Count; }
+			}
+
+			bool ICollection.IsSynchronized
+			{
+				get { return false; }
+			}
+
+			Object ICollection.SyncRoot
+			{
+				get { return this; }
+			}
+
+			// Implement the IEnumerable interface.
+			public IEnumerator GetEnumerator()
+			{
+				return List.GetEnumerator();
+			}
+
+			// Determine if the collection is read-only.
+			public bool IsReadOnly
+			{
+				get { return false; }
+			}
+
+			bool IList.IsFixedSize
+			{
+				get { return false; }
+			}
+
+			// Get the array list that underlies this collection
+			Object IList.this[int index]
+			{
+				get { return List[index]; }
+				set { List[index] = value; }
+			}
+
+			public StatusBarPanel this[int index]
+			{
+				get { return (StatusBarPanel)List[index]; }
+				set { List[index] = value; }
+			}
+
+			protected virtual ArrayList List
+			{
+				get { return list; }
+			}
+
+			int IList.Add(Object value)
+			{
+				int result;
+				result =  List.Add(value);
+				owner.Invalidate();
+				return result;
+			}
+
+			public virtual int Add(StatusBarPanel value)
+			{
+				return List.Add(value);
+			}
+
+			public virtual StatusBarPanel Add(string text)
+			{
+				StatusBarPanel panel = new StatusBarPanel();
+				panel.Text = text;
+				List.Add(panel);
+				return panel;
+			}
+
+			public virtual void AddRange(StatusBarPanel[] panels)
+			{
+				List.AddRange(panels);
+				owner.Invalidate();
+			}
+
+			public virtual void Clear()
+			{
+				List.Clear();
+				owner.Invalidate();
+			}
+
+			bool IList.Contains(Object value)
+			{
+				return List.Contains(value);
+			}
+
+			public bool Contains(StatusBarPanel panel)
+			{
+				return List.Contains(panel);
+			}
+
+			int IList.IndexOf(Object value)
+			{
+				return List.IndexOf(value);
+			}
+
+			public int IndexOf(StatusBarPanel panel)
+			{
+				return List.IndexOf(panel);
+			}
+
+			void IList.Insert(int index, Object value)
+			{
+				List.Insert(index, value);
+				owner.Invalidate();
+			}
+
+			public virtual void Insert(int index, StatusBarPanel value)
+			{
+				List.Insert(index, value);
+			}
+
+			void IList.Remove(Object value)
+			{
+				List.Remove(value);
+				owner.Invalidate();
+			}
+
+			public virtual void Remove(StatusBarPanel value)
+			{
+				List.Remove(value);
+			}
+
+			void IList.RemoveAt(int index)
+			{
+				List.RemoveAt(index);
+			}
+		}
 	}
 	/*
 	public class StatusBarDrawItemEventArgs : DrawItemEventArgs

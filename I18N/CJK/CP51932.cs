5e8142e464494aa33e7f4e105fb601fc876ce8d8
Rhys Weatherley <rweather@southern-storm.com.au>
2003-08-13 04:08:45 +0000

    Add code page 51932 (euc-jp) to the I18N routines.

--- I18N/CJK/CP51932.cs
+++ I18N/CJK/CP51932.cs
@@ -1,7 +1,7 @@
 /*
- * CP932.cs - Japanese (Shift-JIS) code page.
+ * CP51932.cs - Japanese (EUC) code page.
  *
- * Copyright (c) 2002  Southern Storm Software, Pty Ltd
+ * Copyright (c) 2002, 2003  Southern Storm Software, Pty Ltd
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,16 +25,16 @@ using System;
 using System.Text;
 using I18N.Common;
 
-public unsafe class CP932 : Encoding
+public unsafe class CP51932 : Encoding
 {
-	// Magic number used by Windows for the Shift-JIS code page.
-	internal const int SHIFTJIS_CODE_PAGE = 932;
+	// Magic number used by Windows for the EUC code page.
+	private const int EUCJP_CODE_PAGE = 51932;
 
 	// Internal state.
 	private JISConvert convert;
 
 	// Constructor.
-	public CP932() : base(SHIFTJIS_CODE_PAGE)
+	public CP51932() : base(EUCJP_CODE_PAGE)
 			{
 				// Load the JIS conversion tables.
 				convert = JISConvert.Convert;
@@ -106,13 +106,7 @@ public unsafe class CP932 : Encoding
 					{
 						// This range contains extra characters,
 						// including half-width katakana.
-						value = (ch - 0xFF01) * 2;
-						value = ((int)(extraToJis[value])) |
-								(((int)(extraToJis[value + 1])) << 8);
-						if(value >= 0x0100)
-						{
-							++length;
-						}
+						++length;
 					}
 				}
 
@@ -190,58 +184,58 @@ public unsafe class CP932 : Encoding
 							switch(ch)
 							{
 								case 0x00A2:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0x91;
+									bytes[posn++] = (byte)0xA1;
+									bytes[posn++] = (byte)(0xA0 + 81);
 									break;
 
 								case 0x00A3:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0x92;
+									bytes[posn++] = (byte)0xA1;
+									bytes[posn++] = (byte)(0xA0 + 82);
 									break;
 
 								case 0x00A7:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0x98;
+									bytes[posn++] = (byte)0xA1;
+									bytes[posn++] = (byte)(0xA0 + 88);
 									break;
 
 								case 0x00A8:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0x4E;
+									bytes[posn++] = (byte)0xA1;
+									bytes[posn++] = (byte)(0xA0 + 37);
 									break;
 
 								case 0x00AC:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0xCA;
+									bytes[posn++] = (byte)0xA2;
+									bytes[posn++] = (byte)(0xA0 + 44);
 									break;
 
 								case 0x00B0:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0x8B;
+									bytes[posn++] = (byte)0xA1;
+									bytes[posn++] = (byte)(0xA0 + 75);
 									break;
 
 								case 0x00B1:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0x7D;
+									bytes[posn++] = (byte)0xA1;
+									bytes[posn++] = (byte)(0xA0 + 62);
 									break;
 
 								case 0x00B4:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0x4C;
+									bytes[posn++] = (byte)0xA1;
+									bytes[posn++] = (byte)(0xA0 + 76);
 									break;
 
 								case 0x00B6:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0xF7;
+									bytes[posn++] = (byte)0xA2;
+									bytes[posn++] = (byte)(0xA0 + 89);
 									break;
 
 								case 0x00D7:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0x7E;
+									bytes[posn++] = (byte)0xA1;
+									bytes[posn++] = (byte)(0xA0 + 63);
 									break;
 
 								case 0x00F7:
-									bytes[posn++] = (byte)0x81;
-									bytes[posn++] = (byte)0x80;
+									bytes[posn++] = (byte)0xA1;
+									bytes[posn++] = (byte)(0xA0 + 64);
 									break;
 							}
 						}
@@ -288,42 +282,42 @@ public unsafe class CP932 : Encoding
 					{
 						bytes[posn++] = (byte)'?';
 					}
-					else if(value < 0x0100)
-					{
-						bytes[posn++] = (byte)value;
-					}
 					else if((posn + 1) >= byteLength)
 					{
 						throw new ArgumentException
 							(Strings.GetString("Arg_InsufficientSpace"),
 							 "bytes");
 					}
+					else if(value < 0x0100)
+					{
+						// Half-width katakana.
+						bytes[posn++] = (byte)0x8E;
+						bytes[posn++] = (byte)value;
+					}
 					else if(value < 0x8000)
 					{
 						// JIS X 0208 character.
 						value -= 0x0100;
-						ch = (value / 0xBC);
-						value = (value % 0xBC) + 0x40;
-						if(value >= 0x7F)
-						{
-							++value;
-						}
-						if(ch < (0x9F - 0x80))
-						{
-							bytes[posn++] = (byte)(ch + 0x81);
-						}
-						else
-						{
-							bytes[posn++] = (byte)(ch - (0x9F - 0x80) + 0xE0);
-						}
-						bytes[posn++] = (byte)value;
+						ch = (value / 94);
+						bytes[posn++] = (byte)(ch + 0xA1);
+						ch = (value % 94);
+						bytes[posn++] = (byte)(ch + 0xA1);
 					}
 					else
 					{
-						// JIS X 0212 character, which Shift-JIS doesn't
-						// support, but we've already allocated two slots.
-						bytes[posn++] = (byte)'?';
-						bytes[posn++] = (byte)'?';
+						// JIS X 0212 character.
+						if((posn + 2) >= byteLength)
+						{
+							throw new ArgumentException
+								(Strings.GetString("Arg_InsufficientSpace"),
+							 	"bytes");
+						}
+						bytes[posn++] = (byte)0x8F;
+						value -= 0x8000;
+						ch = (value / 94);
+						bytes[posn++] = (byte)(ch + 0xA1);
+						ch = (value % 94);
+						bytes[posn++] = (byte)(ch + 0xA1);
 					}
 				}
 
@@ -364,25 +358,42 @@ public unsafe class CP932 : Encoding
 						// single-byte Yen or overline signs.
 						continue;
 					}
-					else if(byteval >= 0xA1 && byteval <= 0xDF)
+					else if(byteval >= 0xA1 && byteval <= 0xFE)
 					{
-						// Half-width katakana.
-						continue;
+						// Two-byte JIS X 0208 character.
+						if(count == 0)
+						{
+							continue;
+						}
+						--count;
+						++index;
 					}
-					else if(byteval < 0x81 ||
-					        (byteval > 0x9F && byteval < 0xE0) ||
-							byteval > 0xEF)
+					else if(byteval == 0x8E)
+					{
+						// Two-byte half-width katakana.
+						if(count == 0)
+						{
+							continue;
+						}
+						--count;
+						++index;
+					}
+					else if(byteval == 0x8F)
+					{
+						// Three-byte JIS X 0212 character.
+						if(count <= 1)
+						{
+							count = 0;
+							continue;
+						}
+						count -= 2;
+						index += 2;
+					}
+					else
 					{
 						// Invalid first byte.
 						continue;
 					}
-					if(count == 0)
-					{
-						// Missing second byte.
-						continue;
-					}
-					++index;
-					--count;
 				}
 
 				// Return the total length.
@@ -424,6 +435,7 @@ public unsafe class CP932 : Encoding
 				int length = 0;
 				int byteval, value;
 				byte *table = convert.jisx0208ToUnicode;
+				byte *table2 = convert.jisx0212ToUnicode;
 				while(byteCount > 0)
 				{
 					byteval = bytes[byteIndex++];
@@ -439,71 +451,127 @@ public unsafe class CP932 : Encoding
 					{
 						// Yen sign.
 						chars[posn++] = '\u00A5';
-						continue;
 					}
 					else if(byteval == 0x7E)
 					{
 						// Overline symbol.
 						chars[posn++] = '\u203E';
-						continue;
 					}
 					else if(byteval < 0x80)
 					{
 						// Ordinary ASCII/Latin1 character.
 						chars[posn++] = (char)byteval;
-						continue;
 					}
-					else if(byteval >= 0xA1 && byteval <= 0xDF)
+					else if(byteval >= 0xA1 && byteval <= 0xFE)
 					{
-						// Half-width katakana.
-						chars[posn++] = (char)(byteval - 0xA1 + 0xFF61);
-						continue;
+						// Two-byte JIS X 0208 character.
+						value = (byteval - 0xA1) * 94;
+						if(byteCount > 0)
+						{
+							byteval = bytes[byteIndex++];
+							--byteCount;
+						}
+						else
+						{
+							byteval = 0;
+						}
+						if(byteval >= 0xA1 && byteval <= 0xFE)
+						{
+							value += (byteval - 0xA1);
+							value *= 2;
+							value = ((int)(table[value])) |
+									(((int)(table[value + 1])) << 8);
+							if(value != 0)
+							{
+								chars[posn++] = (char)value;
+							}
+							else
+							{
+								chars[posn++] = '?';
+							}
+						}
+						else
+						{
+							// Invalid second byte.
+							chars[posn++] = '?';
+						}
 					}
-					else if(byteval >= 0x81 && byteval <= 0x9F)
+					else if(byteval == 0x8E)
 					{
-						value = (byteval - 0x81) * 0xBC;
+						// Two-byte half-width katakana.
+						if(byteCount > 0)
+						{
+							byteval = bytes[byteIndex++];
+							--byteCount;
+						}
+						else
+						{
+							byteval = 0;
+						}
+						if(byteval >= 0xA1 && byteval <= 0xFE)
+						{
+							chars[posn++] = (char)(byteval - 0xA1 + 0xFF61);
+						}
+						else
+						{
+							// Invalid second byte.
+							chars[posn++] = '?';
+						}
 					}
-					else if(byteval >= 0xE0 && byteval <= 0xEF)
+					else if(byteval == 0x8F)
 					{
-						value = (byteval - 0xE0 + (0xA0 - 0x81)) * 0xBC;
+						// Three-byte JIS X 0212 character.
+						if(byteCount > 0)
+						{
+							byteval = bytes[byteIndex++];
+							--byteCount;
+						}
+						else
+						{
+							byteval = 0;
+						}
+						if(byteval >= 0xA1 && byteval <= 0xFE)
+						{
+							value = (byteval - 0xA1) * 94;
+							if(byteCount > 0)
+							{
+								byteval = bytes[byteIndex++];
+								--byteCount;
+							}
+							else
+							{
+								byteval = 0;
+							}
+							if(byteval >= 0xA1 && byteval <= 0xFE)
+							{
+								value += (byteval - 0xA1);
+								value *= 2;
+								value = ((int)(table2[value])) |
+										(((int)(table2[value + 1])) << 8);
+								if(value != 0)
+								{
+									chars[posn++] = (char)value;
+								}
+								else
+								{
+									chars[posn++] = '?';
+								}
+							}
+							else
+							{
+								// Invalid third byte.
+								chars[posn++] = '?';
+							}
+						}
+						else
+						{
+							// Invalid second byte.
+							chars[posn++] = '?';
+						}
 					}
 					else
 					{
 						// Invalid first byte.
-						chars[posn++] = '?';
-						continue;
-					}
-					if(byteCount == 0)
-					{
-						// Missing second byte.
-						chars[posn++] = '?';
-						continue;
-					}
-					byteval = bytes[byteIndex++];
-					--byteCount;
-					if(byteval >= 0x40 && byteval <= 0x7E)
-					{
-						value += (byteval - 0x40);
-					}
-					else if(byteval >= 0x80 && byteval <= 0xFC)
-					{
-						value += (byteval - 0x80 + 0x3F);
-					}
-					else
-					{
-						// Invalid second byte.
-						chars[posn++] = '?';
-						continue;
-					}
-					value *= 2;
-					value = ((int)(table[value])) |
-							(((int)(table[value + 1])) << 8);
-					if(value != 0)
-					{
-						chars[posn++] = (char)value;
-					}
-					else
-					{
 						chars[posn++] = '?';
 					}
 				}
@@ -522,7 +590,7 @@ public unsafe class CP932 : Encoding
 						("charCount",
 						 Strings.GetString("ArgRange_NonNegative"));
 				}
-				return charCount * 2;
+				return charCount * 3;
 			}
 
 	// Get the maximum number of characters needed to decode a
@@ -541,7 +609,7 @@ public unsafe class CP932 : Encoding
 	// Get a decoder that handles a rolling Shift-JIS state.
 	public override Decoder GetDecoder()
 			{
-				return new CP932Decoder(convert);
+				return new CP51932Decoder(convert);
 			}
 
 #if !ECMA_COMPAT
@@ -551,7 +619,7 @@ public unsafe class CP932 : Encoding
 			{
 				get
 				{
-					return "iso-2022-jp";
+					return "euc-jp";
 				}
 			}
 
@@ -560,7 +628,7 @@ public unsafe class CP932 : Encoding
 			{
 				get
 				{
-					return "Japanese (Shift-JIS)";
+					return "Japanese (EUC)";
 				}
 			}
 
@@ -569,7 +637,7 @@ public unsafe class CP932 : Encoding
 			{
 				get
 				{
-					return "iso-2022-jp";
+					return "euc-jp";
 				}
 			}
 
@@ -614,7 +682,7 @@ public unsafe class CP932 : Encoding
 			{
 				get
 				{
-					return "shift_jis";
+					return "euc-jp";
 				}
 			}
 
@@ -623,23 +691,25 @@ public unsafe class CP932 : Encoding
 			{
 				get
 				{
-					return SHIFTJIS_CODE_PAGE;
+					return CP932.SHIFTJIS_CODE_PAGE;
 				}
 			}
 
 #endif // !ECMA_COMPAT
 
-	// Decoder that handles a rolling Shift-JIS state.
-	private sealed class CP932Decoder : Decoder
+	// Decoder that handles a rolling EUC-JP state.
+	private sealed class CP51932Decoder : Decoder
 	{
 		private JISConvert convert;
-		private int lastByte;
+		private int lastByte1;
+		private int lastByte2;
 
 		// Constructor.
-		public CP932Decoder(JISConvert convert)
+		public CP51932Decoder(JISConvert convert)
 				{
 					this.convert = convert;
-					this.lastByte = 0;
+					this.lastByte1 = 0;
+					this.lastByte2 = 0;
 				}
 
 		// Override inherited methods.
@@ -664,25 +734,48 @@ public unsafe class CP932 : Encoding
 					// Determine the total length of the converted string.
 					int length = 0;
 					int byteval;
-					int last = lastByte;
+					int last1 = lastByte1;
+					int last2 = lastByte2;
 					while(count > 0)
 					{
 						byteval = bytes[index++];
 						--count;
-						if(last == 0)
+						if(last1 == 0)
 						{
-							if((byteval >= 0x81 && byteval <= 0x9F) ||
-							   (byteval >= 0xE0 && byteval <= 0xEF))
+							if((byteval >= 0xA1 && byteval <= 0xFE) ||
+							   byteval == 0x8E || byteval == 0x8F)
 							{
 								// First byte in a double-byte sequence.
-								last = byteval;
+								last1 = byteval;
 							}
 							++length;
+						}
+						else if(last1 == 0x8F)
+						{
+							if(last2 == 0)
+							{
+								// Second byte in a triple-byte sequence.
+								if(byteval >= 0xA1 && byteval <= 0xFE)
+								{
+									last2 = byteval;
+								}
+								else
+								{
+									last1 = 0;
+								}
+							}
+							else
+							{
+								// Third byte in a triple-byte sequence.
+								last1 = 0;
+								last2 = 0;
+							}
 						}
 						else
 						{
 							// Second byte in a double-byte sequence.
-							last = 0;
+							last1 = 0;
+							last2 = 0;
 						}
 					}
 	
@@ -722,13 +815,15 @@ public unsafe class CP932 : Encoding
 					int posn = charIndex;
 					int charLength = chars.Length;
 					int byteval, value;
-					int last = lastByte;
+					int last1 = lastByte1;
+					int last2 = lastByte2;
 					byte *table = convert.jisx0208ToUnicode;
+					byte *table2 = convert.jisx0212ToUnicode;
 					while(byteCount > 0)
 					{
 						byteval = bytes[byteIndex++];
 						--byteCount;
-						if(last == 0)
+						if(last1 == 0)
 						{
 							if(posn >= charLength)
 							{
@@ -736,11 +831,11 @@ public unsafe class CP932 : Encoding
 									(Strings.GetString
 										("Arg_InsufficientSpace"), "chars");
 							}
-							if((byteval >= 0x81 && byteval <= 0x9F) ||
-							   (byteval >= 0xE0 && byteval <= 0xEF))
+							if((byteval >= 0xA1 && byteval <= 0xFE) ||
+							   byteval == 0x8E || byteval == 0x8F)
 							{
 								// First byte in a double-byte sequence.
-								last = byteval;
+								last1 = byteval;
 							}
 							else if(byteval == 0x5C)
 							{
@@ -757,43 +852,78 @@ public unsafe class CP932 : Encoding
 								// Ordinary ASCII/Latin1 character.
 								chars[posn++] = (char)byteval;
 							}
-							else if(byteval >= 0xA1 && byteval <= 0xDF)
-							{
-								// Half-width katakana character.
-								chars[posn++] = (char)(byteval - 0xA1 + 0xFF61);
-							}
 							else
 							{
 								// Invalid first byte.
 								chars[posn++] = '?';
 							}
 						}
-						else
+						else if(last1 == 0x8F)
 						{
-							// Second byte in a double-byte sequence.
-							if(last >= 0x81 && last <= 0x9F)
+							if(last2 == 0)
 							{
-								value = (last - 0x81) * 0xBC;
+								// Second byte in a triple-byte sequence.
+								if(byteval < 0xA1 || byteval > 0xFE)
+								{
+									// Invalid second byte.
+									chars[posn++] = '?';
+									last1 = 0;
+									continue;
+								}
+								last2 = byteval;
 							}
 							else
 							{
-								value = (last - 0xE0 + (0xA0 - 0x81)) * 0xBC;
+								// Third byte in a triple-byte sequence.
+								if(byteval < 0xA1 || byteval > 0xFE)
+								{
+									// Invalid second byte.
+									chars[posn++] = '?';
+									last1 = 0;
+									last2 = 0;
+									continue;
+								}
+								value = (last2 - 0xA1) * 94 + (byteval - 0xA1);
+								value *= 2;
+								value = ((int)(table2[value])) |
+										(((int)(table2[value + 1])) << 8);
+								if(value != 0)
+								{
+									chars[posn++] = (char)value;
+								}
+								else
+								{
+									chars[posn++] = '?';
+								}
+								last1 = 0;
+								last2 = 0;
 							}
-							last = 0;
-							if(byteval >= 0x40 && byteval <= 0x7E)
+						}
+						else if(last1 == 0x8E)
+						{
+							// Second byte in a half-width katakana sequence.
+							if(byteval >= 0xA1 && byteval <= 0xDF)
 							{
-								value += (byteval - 0x40);
-							}
-							else if(byteval >= 0x80 && byteval <= 0xFC)
-							{
-								value += (byteval - 0x80 + 0x3F);
+								chars[posn++] = (char)(byteval - 0xA1 + 0xFF61);
 							}
 							else
 							{
 								// Invalid second byte.
 								chars[posn++] = '?';
+							}
+							last1 = 0;
+						}
+						else
+						{
+							// Second byte in a double-byte sequence.
+							if(byteval < 0xA1 || byteval > 0xFE)
+							{
+								// Invalid second byte.
+								chars[posn++] = '?';
+								last1 = 0;
 								continue;
 							}
+							value = (last1 - 0xA1) * 94 + (byteval - 0xA1);
 							value *= 2;
 							value = ((int)(table[value])) |
 									(((int)(table[value + 1])) << 8);
@@ -805,22 +935,24 @@ public unsafe class CP932 : Encoding
 							{
 								chars[posn++] = '?';
 							}
+							last1 = 0;
 						}
 					}
-					lastByte = last;
+					lastByte1 = last1;
+					lastByte2 = last2;
 
 					// Return the final length to the caller.
 					return posn - charIndex;
 				}
 
-	} // class CP932Decoder
+	} // class CP51932Decoder
 
-}; // class CP932
+}; // class CP51932
 
-public class ENCshift_jis : CP932
+public class ENCeuc_jp : CP51932
 {
-	public ENCshift_jis() : base() {}
+	public ENCeuc_jp() : base() {}
 
-}; // class ENCshift_jis
+}; // class ENCeuc_jp
 
 }; // namespace I18N.CJK

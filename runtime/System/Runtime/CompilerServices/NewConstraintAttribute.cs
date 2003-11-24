86ce3e0f940b3f3b6206d53e0097663c627d9379
Rhys Weatherley <rweather@southern-storm.com.au>
2003-11-24 04:08:35 +0000

    Add the "NewConstraintAttribute" class, and the attribute flag bit for generic parameters.

--- runtime/System/Runtime/CompilerServices/NewConstraintAttribute.cs
+++ runtime/System/Runtime/CompilerServices/NewConstraintAttribute.cs
@@ -1,7 +1,8 @@
 /*
- * AttributeTargets.cs - Implementation of the "System.AttributeTargets" class.
+ * NewConstraintAttribute.cs - Implementation of the
+ *	"System.Runtime.CompilerServices.NewConstraintAttribute" class.
  *
- * Copyright (C) 2001  Southern Storm Software, Pty Ltd.
+ * Copyright (C) 2003  Southern Storm Software, Pty Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,33 +19,19 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-namespace System
+namespace System.Runtime.CompilerServices
 {
 
-[Flags]
-public enum AttributeTargets
-{
-	Assembly		 = 0x0001,
-	Module			 = 0x0002,
-	Class			 = 0x0004,
-	Struct			 = 0x0008,
-	Enum			 = 0x0010,
-	Constructor		 = 0x0020,
-	Method			 = 0x0040,
-	Property		 = 0x0080,
-	Field			 = 0x0100,
-	Event			 = 0x0200,
-	Interface		 = 0x0400,
-	Parameter		 = 0x0800,
-	Delegate		 = 0x1000,
-	ReturnValue		 = 0x2000,
 #if CONFIG_GENERICS
-	GenericParameter = 0x4000,
-	All				 = 0x7FFF
-#else
-	All				 = 0x3FFF
-#endif
 
-}; // enum AttributeTargets
+[AttributeUsage(AttributeTargets.GenericParameter)]
+public sealed class NewConstraintAttribute : Attribute
+{
+	// Constructor.
+	public NewConstraintAttribute() {}
 
-}; // namespace System
+}; // class NewConstraintAttribute
+
+#endif // CONFIG_GENERICS
+
+}; // namespace System.Runtime.CompilerServices

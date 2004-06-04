218ac34986eb5143145b95be255a0d2e97c1a977
Gopal V <gopalv82@yahoo.com>
2004-06-04 14:09:46 +0000

    add error provider class and component model  checks for HelpProvider

--- System.Windows.Forms/HelpProvider.cs
+++ System.Windows.Forms/HelpProvider.cs
@@ -1,8 +1,29 @@
+/*
+ * HelpProvider.cs - Implementation of "ErrorProvider" class
+ *
+ * Copyright (C) 2004  Southern Storm Software, Pty Ltd.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR   See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, , 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 using System;
 using System.Drawing;
 
 namespace System.Windows.Forms
 {
+#if CONFIG_COMPONENT_MODEL
 	public class HelpProvider : System.ComponentModel.Component
 	{
 		private String helpNamespace;
@@ -118,4 +139,5 @@ namespace System.Windows.Forms
 
 	}
 
+#endif /* CONFIG_COMPONENT_MODEL */
 }

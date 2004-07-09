5f4336d6cbe0ef533dd43ae813f967ecae6eed7f
Rhys Weatherley <rweather@southern-storm.com.au>
2004-07-09 11:04:21 +0000

    Implement some of the VB financial functions, using kspread as a guide to how they should function.

--- Basic/Financial.cs
+++ Basic/Financial.cs
@@ -33,36 +33,90 @@ public sealed class Financial
 	// This class cannot be instantiated.
 	private Financial() {}
 
-	// Calculate depreciation.
-	[TODO]
+	// Utility functions (based on kspread).
+	static double getPay(double rate, double nper, double pv,
+						 double fv, double type)
+			{
+				double pvif, fvifa;
+				pvif = Math.Pow(1 + rate, nper);
+				fvifa = (Math.Pow(1 + rate, nper) - 1.0) / rate;
+				return ((-pv * pvif - fv) / ((1.0 + rate * type) * fvifa));
+			}
+	static double getPrinc(double start, double pay,
+						   double rate, double period)
+			{
+				return (start * Math.Pow(1.0 + rate, period) +
+					pay * ((Math.Pow(1 + rate, period) - 1) / rate));
+			}
+
+	// Calculate depreciation (algorithm from kspread).
 	public static double DDB(double Cost, double Salvage,
 							 double Life, double Period,
 							 [Optional] [DefaultValue(2.0)] double Factor)
 			{
-				// TODO
-				return 0.0;
+				if(Cost < 0.0 || Salvage < 0.0 || Life <= 0.0 ||
+				   Period < 0.0 || Factor < 0.0)
+				{
+					throw new ArgumentException(S._("VB_InvalidDDB"));
+				}
+				double total = 0.0;
+				for(double i = 0.0; i < Life; i += 1.0)
+				{
+					double periodDep = (Cost - total) * (Factor / Life);
+					if((i + 1.0) >= Period)
+					{
+						return periodDep;
+					}
+					else
+					{
+						total += periodDep;
+					}
+				}
+				return Cost - total - Salvage;
 			}
 
 	// Calculate future value.
-	[TODO]
-	public static double FV(double Rate, double Nper, double Pmt,
+	public static double FV(double Rate, double NPer, double Pmt,
 							 [Optional] [DefaultValue(0.0)] double PV,
 							 [Optional] [DefaultValue(DueDate.EndOfPeriod)]
 							 		DueDate Due)
 			{
-				// TODO
-				return 0.0;
+				double rate1, endRate, temp;
+				if(Rate == 0.0)
+				{
+					return -PV - NPer * Pmt;
+				}
+				rate1 = Rate + 1.0;
+				if(Due == DueDate.EndOfPeriod)
+				{
+					endRate = 1.0;
+				}
+				else
+				{
+					endRate = rate1;
+				}
+				temp = Math.Pow(rate1, NPer);
+				return -PV * temp - (Pmt / Rate * endRate) * (temp - 1.0);
 			}
 
-	// Calculate interest payment.
-	[TODO]
+	// Calculate interest payment (algorithm from kspread).
 	public static double IPmt(double Rate, double Per, double NPer, double PV,
 							  [Optional] [DefaultValue(0.0)] double FV,
 							  [Optional] [DefaultValue(DueDate.EndOfPeriod)]
 							 		DueDate Due)
 			{
-				// TODO
-				return 0.0;
+				double type;
+				if(Due == DueDate.EndOfPeriod)
+				{
+					type = 0.0;
+				}
+				else
+				{
+					type = 1.0;
+				}
+				double payment = getPay(Rate, NPer, PV, FV, type);
+				double ipmt = -(getPrinc(PV, payment, Rate, Per - 1.0));
+				return ipmt * Rate;
 			}
 
 	// Calculate internal rate of return.
@@ -83,15 +137,33 @@ public sealed class Financial
 				return 0.0;
 			}
 
-	// Calculate number of periods.
-	[TODO]
+	// Calculate number of periods (algorithm from kspread).
 	public static double NPer(double Rate, double Pmt, double PV,
 							  [Optional] [DefaultValue(0.0)] double FV,
 							  [Optional] [DefaultValue(DueDate.EndOfPeriod)]
 							 		DueDate Due)
 			{
-				// TODO
-				return 0.0;
+				if(Rate <= 0.0)
+				{
+					throw new ArgumentException(S._("VB_InvalidNPer"));
+				}
+				double type;
+				if(Due == DueDate.EndOfPeriod)
+				{
+					type = 0.0;
+				}
+				else
+				{
+					type = 1.0;
+				}
+				double d = Pmt * (1.0 + Rate * type) - FV * Rate;
+				double d2 = PV * Rate + Pmt * (1.0 + Rate * type);
+				double res = d / d2;
+				if(res <= 0.0)
+				{
+					throw new ArgumentException(S._("VB_InvalidNPer"));
+				}
+				return Math.Log(res) / Math.Log(1.0 + Rate);
 			}
 
 	// Calculate net present value.
@@ -103,25 +175,41 @@ public sealed class Financial
 			}
 
 	// Calculate payment.
-	[TODO]
 	public static double Pmt(double Rate, double NPer, double PV,
 							 [Optional] [DefaultValue(0.0)] double FV,
 							 [Optional] [DefaultValue(DueDate.EndOfPeriod)]
 							 		DueDate Due)
 			{
-				// TODO
-				return 0.0;
+				double type;
+				if(Due == DueDate.EndOfPeriod)
+				{
+					type = 0.0;
+				}
+				else
+				{
+					type = 1.0;
+				}
+				return getPay(Rate, NPer, PV, FV, type);
 			}
 
-	// Calculate principal payment.
-	[TODO]
+	// Calculate principal payment (algorithm from kspread).
 	public static double PPmt(double Rate, double Per, double NPer, double PV,
 							  [Optional] [DefaultValue(0.0)] double FV,
 							  [Optional] [DefaultValue(DueDate.EndOfPeriod)]
 							 		DueDate Due)
 			{
-				// TODO
-				return 0.0;
+				double type;
+				if(Due == DueDate.EndOfPeriod)
+				{
+					type = 0.0;
+				}
+				else
+				{
+					type = 1.0;
+				}
+				double pay = getPay(Rate, NPer, PV, FV, type);
+				double ipmt = (-getPrinc(PV, pay, Rate, Per - 1.0)) * Rate;
+				return pay - ipmt;
 			}
 
 	// Calculate present value.
@@ -147,21 +235,26 @@ public sealed class Financial
 				return 0.0;
 			}
 
-	// Calculate straight-line depreciation.
-	[TODO]
+	// Calculate straight-line depreciation (algorithm from kspread).
 	public static double SLN(double Cost, double Salvage, double Life)
 			{
-				// TODO
-				return 0.0;
+				if(Life <= 0.0)
+				{
+					throw new ArgumentException(S._("VB_InvalidSLN"));
+				}
+				return (Cost - Salvage) / Life;
 			}
 
-	// Calculate sum of years for depreciation.
-	[TODO]
+	// Calculate sum of years for depreciation (algorithm from kspread).
 	public static double SYD(double Cost, double Salvage,
 							 double Life, double Period)
 			{
-				// TODO
-				return 0.0;
+				if(Life <= 0.0)
+				{
+					throw new ArgumentException(S._("VB_InvalidSYD"));
+				}
+				return ((Cost - Salvage) / (Life - Period + 1.0) * 2.0) /
+							(Life * (Life + 1.0));
 			}
 
 }; // class Financial

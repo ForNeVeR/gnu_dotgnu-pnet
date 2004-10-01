ef5aa6e93f9150cde51f25c2306af3548922942d
Gopal V <gopalv82@yahoo.com>
2004-10-01 13:42:09 +0000

    implement parts of XmlDocumentNavigator

--- System.Xml/Private/XmlDocumentNavigator.cs
+++ System.Xml/Private/XmlDocumentNavigator.cs
@@ -1,3 +1,24 @@
+/*
+ * XmlDocumentNavigator.cs - Implementation of the
+ *		"System.Xml.Private.XmlDocumentNavigator" class.
+ *
+ * Copyright (C) 2004  Southern Storm Software, Pty Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 namespace System.Xml.Private
 {
 
@@ -8,8 +29,14 @@ using System.Xml.XPath;
 
 internal class XmlDocumentNavigator : XPathNavigator
 {
+	private XmlNode node;
+	private XmlDocument document;
+
 	public XmlDocumentNavigator(XmlNode node) : base()
 	{
+		this.node = node;
+		this.document = (node is XmlDocument) ? 
+							(XmlDocument)node : node.OwnerDocument;
 	}
 
 	public override XPathNavigator Clone()
@@ -25,36 +52,76 @@ internal class XmlDocumentNavigator : XPathNavigator
 
 	public override String GetNamespace(String name)
 	{
-		return null;
+		return node.GetNamespaceOfPrefix(name);
 	}
 	
 	public override bool IsSamePosition(XPathNavigator other)
 	{
-		return false;
+		XmlDocumentNavigator nav = (other as XmlDocumentNavigator);
+		return ((nav != null) && (nav.node == node));
 	}
 	
 	public override bool MoveTo(XPathNavigator other)
 	{
+		XmlDocumentNavigator nav = (other as XmlDocumentNavigator);
+		if(nav != null)
+		{
+			node = nav.node;
+			document = nav.document;
+			return true;
+		}
 		return false;
 	}
 
 	public override bool MoveToAttribute(String localName, String namespaceURI)
 	{
+		if(node.Attributes != null)
+		{
+			foreach(XmlAttribute attr in node.Attributes)
+			{
+				// TODO : can this be an Object Ref compare ?
+				if(attr.LocalName == localName && 
+					attr.NamespaceURI == namespaceURI)
+				{
+					node = attr;
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 
 	public override bool MoveToFirst()
 	{
+		// TODO : is this correct ?. Will a Text qualify as a first node ?
+		if(node.NodeType != XmlNodeType.Attribute && node.ParentNode != null)
+		{
+			node = node.ParentNode.FirstChild;
+			return true;
+		}
 		return false;
 	}
 
 	public override bool MoveToFirstAttribute()
 	{
+		if(NodeType == XPathNodeType.Element && node.Attributes != null)
+		{
+			if(node.Attributes.Count != 0)
+			{
+				node = node.Attributes[0];
+			}
+			return true;
+		}
 		return false;
 	}
 
 	public override bool MoveToFirstChild()
 	{
+		//TODO: handled DTD entries
+		if(node.FirstChild != null)
+		{
+			node = node.FirstChild;
+		}
 		return false;
 	}
 
@@ -75,11 +142,51 @@ internal class XmlDocumentNavigator : XPathNavigator
 
 	public override bool MoveToNext()
 	{
+		XmlNode next = node.NextSibling;
+		// TODO: implement normalization
+		if(next	!= null)
+		{
+			node = next;
+			return true;	
+		}
 		return false;
 	}
 
 	public override bool MoveToNextAttribute()
 	{
+		if(NodeType == XPathNodeType.Attribute)
+		{
+			int i;
+			XmlElement owner = ((XmlAttribute)node).OwnerElement;
+
+			if(owner == null)
+			{
+				return false;
+			}
+			
+			XmlAttributeCollection list = owner.Attributes;
+			
+			if(list == null)
+			{
+				return false;
+			}
+
+			for(i=0 ; i<list.Count ; i++)
+			{
+				// This should be a lot faster
+				if(((Object)list[i]) == ((Object)node))
+				{
+					break;
+				}
+			}
+
+			if(i != list.Count)
+			{
+				node = list[i];
+			}
+
+			return true;
+		}
 		return false;
 	}
 
@@ -91,16 +198,40 @@ internal class XmlDocumentNavigator : XPathNavigator
 
 	public override bool MoveToParent()
 	{
+		if(node.NodeType == XmlNodeType.Attribute)
+		{
+			XmlElement owner = ((XmlAttribute)node).OwnerElement;
+			if(owner != null)
+			{
+				node = owner;
+				return true;
+			}
+		}
+		else if (node.ParentNode != null)
+		{
+			node = node.ParentNode;
+			return true;
+		}
 		return false;
 	}
 
 	public override bool MoveToPrevious()
 	{
+		if(node.PreviousSibling != null)
+		{
+			node = node.PreviousSibling;
+			return true;
+		}
 		return false;
 	}
 
 	public override void MoveToRoot()
 	{
+		// TODO: make sure we don't use this for fragments
+		if(document != null && document.DocumentElement != null)
+		{
+			node = document.DocumentElement;
+		}
 		return;
 	}
 	
@@ -108,7 +239,7 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
-			return null;
+			return node.BaseURI;
 		}
 	}
 
@@ -116,6 +247,10 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
+			if(node.Attributes != null)
+			{
+				return (node.Attributes.Count != 0);
+			}
 			return false;
 		}
 	}
@@ -124,7 +259,7 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
-			return false;
+			return (node.FirstChild != null);	
 		}
 	}
 
@@ -132,6 +267,10 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
+			if(node.NodeType == XmlNodeType.Element)
+			{
+				return ((XmlElement)node).IsEmpty;
+			}
 			return false;
 		}
 	}
@@ -140,7 +279,16 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
-			return null;
+			XPathNodeType nodeType = NodeType;
+			
+			if(nodeType == XPathNodeType.Element ||
+				nodeType == XPathNodeType.Attribute ||
+				nodeType == XPathNodeType.ProcessingInstruction ||
+				nodeType == XPathNodeType.Namespace)
+			{
+				return node.LocalName;
+			}
+			return String.Empty;
 		}
 	}
 
@@ -148,7 +296,16 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
-			return null;
+			XPathNodeType nodeType = NodeType;
+			
+			if(nodeType == XPathNodeType.Element ||
+				nodeType == XPathNodeType.Attribute ||
+				nodeType == XPathNodeType.ProcessingInstruction ||
+				nodeType == XPathNodeType.Namespace)
+			{
+				return node.Name;
+			}
+			return String.Empty;
 		}
 	}
 
@@ -156,7 +313,7 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
-			return null;
+			return document.NameTable;
 		}
 	}
 
@@ -164,7 +321,7 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
-			return null;
+			return node.NamespaceURI;
 		}
 	}
 
@@ -172,7 +329,53 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
-			return XPathNodeType.All;
+			switch(node.NodeType)
+			{
+				case XmlNodeType.Element:
+				{
+					return XPathNodeType.Element;
+				}
+				break;
+				case XmlNodeType.Comment:
+				{
+					return XPathNodeType.Comment;
+				}
+				break;
+				case XmlNodeType.Attribute:
+				{
+					return XPathNodeType.Attribute;
+				}
+				break;
+				case XmlNodeType.Text:
+				{
+					return XPathNodeType.Text;
+				}
+				break;
+				case XmlNodeType.Whitespace:
+				{
+					return XPathNodeType.Whitespace;
+				}
+				break;
+				case XmlNodeType.SignificantWhitespace:
+				{
+					return XPathNodeType.SignificantWhitespace;
+				}
+				break;
+				case XmlNodeType.ProcessingInstruction:
+				{
+					return XPathNodeType.ProcessingInstruction;
+				}
+				break;
+				case XmlNodeType.Document:
+				{
+					return XPathNodeType.Root;
+				}
+				break;
+			}
+			// TODO resources
+			throw new InvalidOperationException(
+				String.Format("Invalid XPathNodeType for: {0}", 
+							 node.NodeType)); 
 		}
 	}
 
@@ -180,7 +383,7 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
-			return null;
+			return node.Prefix;
 		}
 	}
 
@@ -188,7 +391,37 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
-			return null;
+			switch(NodeType)
+			{
+				case XPathNodeType.Attribute:
+				case XPathNodeType.Comment:
+				case XPathNodeType.ProcessingInstruction:
+				{
+					return node.Value;
+				}
+				break;
+				case XPathNodeType.Text:
+				case XPathNodeType.Whitespace:
+				case XPathNodeType.SignificantWhitespace:
+				{
+					// TODO : normalize
+					return node.Value;
+				}
+				break;
+				case XPathNodeType.Element:
+				case XPathNodeType.Root:
+				{
+					return node.InnerText;
+				}
+				break;
+				case XPathNodeType.Namespace:
+				{
+					// TODO ?
+					return String.Empty;
+				}
+				break;
+			}
+			return String.Empty;
 		}
 	}
 
@@ -196,7 +429,7 @@ internal class XmlDocumentNavigator : XPathNavigator
 	{
 		get
 		{
-			return null;
+			return String.Empty;
 		}
 	}
 }

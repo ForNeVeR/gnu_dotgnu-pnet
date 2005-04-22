0794cb4b099d8f085892595e8ae70f3c78d0a618
Marc Haisenko <marc@darkdust.net>
2005-04-22 16:13:19 +0000

    * DotGNU.Misc: added new namespace
    * Makefile.am, DotGNU.Misc/Makefile.am, DotGNU.Misc/Misc.build,
      DotGNU.Misc/Profiling.cs: added class with static members for
      controling the enhanced profiler that I've just checked into pnet.
      I'll add some comments on Monday.

---
+++ DotGNU.Misc/Profiling.cs
@@ -0,0 +1,45 @@
+/*
+ * Profiling.cs - Implementation of the "DotGNU.Misc.Profiling" class.
+ *
+ * Copyright (C) 2003  Southern Storm Software, Pty Ltd.
+ *
+ * This program is free software, you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+namespace DotGNU.Misc
+{
+
+using System;
+using System.Runtime.CompilerServices;
+
+public sealed class Profiling
+{
+	// This class cannot be instantiated.
+	private Profiling() {}
+
+	[MethodImpl(MethodImplOptions.InternalCall)]
+        extern public static void StartProfiling();
+	
+	[MethodImpl(MethodImplOptions.InternalCall)]
+        extern public static void StopProfiling();
+	
+	[MethodImpl(MethodImplOptions.InternalCall)]
+        extern public static bool IsProfilingEnabled();
+	
+	[MethodImpl(MethodImplOptions.InternalCall)]
+        extern public static bool IsProfilingSupported();
+}; // class Profiling
+
+}; // namespace DotGNU.Misc

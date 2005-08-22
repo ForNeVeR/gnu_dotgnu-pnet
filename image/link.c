fd16f6ad1600bf23c635d7706400dbede9612df6
Gopal V <gopalv82@yahoo.com>
2005-08-22 06:50:37 +0000

    Add various lib64 paths to the shared library lookup path for amd64

--- image/link.c
+++ image/link.c
@@ -30,8 +30,16 @@ extern	"C" {
  */
 #define	CSCC_LIB_PATH_DEFAULT	\
 			"/usr/local/lib/cscc/lib:/usr/lib/cscc/lib"
+#if defined(__x86_64) || defined(__x86_64__)
+/* Note: x86_64 is a special case as it has 32 bit binaries
+		 in /lib and 64 bit in /lib64*/
+#define	LD_LIBRARY_PATH_DEFAULT	\
+			("/usr/local/lib64:/usr/X11R6/lib64:/usr/lib64" \
+			"/usr/local/lib:/usr/X11R6/lib:/usr/lib")
+#else
 #define	LD_LIBRARY_PATH_DEFAULT	\
 			"/usr/local/lib:/usr/X11R6/lib:/usr/lib"
+#endif
 
 /*
  * The cached system path, loaded from the environment.  The "important"

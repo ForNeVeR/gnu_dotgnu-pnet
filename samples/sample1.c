7fee7ec2dcdd3e404fd5db46f3b93079322e196b
Deryk Robosson <deryk@0x0a.com>
2005-12-11 13:10:07 +0000

    display a simple x window for now, color later with Crayons.

--- samples/sample1.c
+++ samples/sample1.c
@@ -18,8 +18,54 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
  
+ #include <Crayons.h>	/* includes X11/Xlib.h if HAVE_X11_XLIB_H is defined */
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ 
  int main()
  {
  	/* TODO */
+	Display *display;
+	int screen_number;
+	Window win;
+	unsigned int disp_width, disp_height;
+	unsigned int width, height;
+	unsigned int win_x, win_y;
+	unsigned int win_border_width;
+	char *display_name = getenv("DISPLAY");
+
+	display = XOpenDisplay(display_name);
+	if(display == NULL)
+	{
+		fprintf(stderr, "cannot connect to Xserver %s\n",
+			display_name);
+		exit(1);
+	}
+
+	screen_number = DefaultScreen(display);
+	disp_width = DisplayWidth(display, screen_number);
+	disp_height = DisplayHeight(display, screen_number);
+	width = disp_width / 3;
+	height = disp_height / 3;
+	win_x = 0;
+	win_y = 0;
+	win_border_width = 2;
+
+	win = XCreateSimpleWindow(display,
+		RootWindow(display, screen_number),
+		win_x, win_y,
+		width, height,
+		win_border_width,
+		BlackPixel(display, screen_number),
+		WhitePixel(display, screen_number));
+
+	XMapWindow(display, win);
+	XSync(display, 0);
+
+	sleep(4);
+
+	XCloseDisplay(display);
  }
  

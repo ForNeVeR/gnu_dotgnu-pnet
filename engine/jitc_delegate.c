dcb65da3d304b78d1f97d9e11ba8db16710080f2
Klaus Treichel <ktreichel@web.de>
2007-02-13 21:28:32 +0000

    2007-02-13  Klaus Treichel  <ktreichel@web.de>
    	* engine/jitc_delegate.c: remove the dumps from the on demand compiler
    	functions because this is handled now in the on demand driver function.
    
    2007-02-13  Robert Schwebel  <r.schwebel@pengutronix.de>
    
    	* libtool.m4, libgc/libtool.m4: Remove auto generated files.

--- engine/jitc_delegate.c
+++ engine/jitc_delegate.c
@@ -464,28 +464,6 @@ static int _ILJitCompileDelegateInvoke(ILJitFunction func)
 
 	/* TODO: Add the code generation here. */
 
-#if !defined(IL_CONFIG_REDUCE_CODE) && !defined(IL_WITHOUT_TOOLS) && defined(_IL_JIT_ENABLE_DEBUG)
-#ifdef _IL_JIT_DUMP_FUNCTION
-	if(jitCoder->flags & IL_CODER_FLAG_STATS)
-	{
-		ILMutexLock(globalTraceMutex);
-		jit_dump_function(stdout, func, methodName);
-		ILMutexUnlock(globalTraceMutex);
-	}
-#endif
-#ifdef _IL_JIT_DISASSEMBLE_FUNCTION
-	if(jitCoder->flags & IL_CODER_FLAG_STATS)
-	{
-		if(!jit_function_compile(func))
-		{
-			return JIT_RESULT_COMPILE_ERROR;
-		}
-		ILMutexLock(globalTraceMutex);
-		jit_dump_function(stdout, func, methodName);
-		ILMutexUnlock(globalTraceMutex);
-	}
-#endif
-#endif
 	return JIT_RESULT_OK;
 }
 
@@ -603,28 +581,6 @@ static int _ILJitCompileDelegateBeginInvoke(ILJitFunction func)
 	jit_insn_return(func, ctorArgs[0]);
 #endif
 
-#if !defined(IL_CONFIG_REDUCE_CODE) && !defined(IL_WITHOUT_TOOLS) && defined(_IL_JIT_ENABLE_DEBUG)
-#ifdef _IL_JIT_DUMP_FUNCTION
-	if(jitCoder->flags & IL_CODER_FLAG_STATS)
-	{
-		ILMutexLock(globalTraceMutex);
-		jit_dump_function(stdout, func, methodName);
-		ILMutexUnlock(globalTraceMutex);
-	}
-#endif
-#ifdef _IL_JIT_DISASSEMBLE_FUNCTION
-	if(jitCoder->flags & IL_CODER_FLAG_STATS)
-	{
-		if(!jit_function_compile(func))
-		{
-			return JIT_RESULT_COMPILE_ERROR;
-		}
-		ILMutexLock(globalTraceMutex);
-		jit_dump_function(stdout, func, methodName);
-		ILMutexUnlock(globalTraceMutex);
-	}
-#endif
-#endif
 	return JIT_RESULT_OK;
 }
 
@@ -737,28 +693,6 @@ static int _ILJitCompileDelegateEndInvoke(ILJitFunction func)
 		return JIT_RESULT_COMPILE_ERROR;
 	}
 
-#if !defined(IL_CONFIG_REDUCE_CODE) && !defined(IL_WITHOUT_TOOLS) && defined(_IL_JIT_ENABLE_DEBUG)
-#ifdef _IL_JIT_DUMP_FUNCTION
-	if(jitCoder->flags & IL_CODER_FLAG_STATS)
-	{
-		ILMutexLock(globalTraceMutex);
-		jit_dump_function(stdout, func, methodName);
-		ILMutexUnlock(globalTraceMutex);
-	}
-#endif
-#ifdef _IL_JIT_DISASSEMBLE_FUNCTION
-	if(jitCoder->flags & IL_CODER_FLAG_STATS)
-	{
-		if(!jit_function_compile(func))
-		{
-			return JIT_RESULT_COMPILE_ERROR;
-		}
-		ILMutexLock(globalTraceMutex);
-		jit_dump_function(stdout, func, methodName);
-		ILMutexUnlock(globalTraceMutex);
-	}
-#endif
-#endif
 	return JIT_RESULT_OK;
 }
 
@@ -836,28 +770,6 @@ static int _ILJitCompileMultiCastDelegateInvoke(ILJitFunction func)
 #endif
 	jit_insn_return(func, returnValue);
 
-#if !defined(IL_CONFIG_REDUCE_CODE) && !defined(IL_WITHOUT_TOOLS) && defined(_IL_JIT_ENABLE_DEBUG)
-#ifdef _IL_JIT_DUMP_FUNCTION
-	if(jitCoder->flags & IL_CODER_FLAG_STATS)
-	{
-		ILMutexLock(globalTraceMutex);
-		jit_dump_function(stdout, func, methodName);
-		ILMutexUnlock(globalTraceMutex);
-	}
-#endif
-#ifdef _IL_JIT_DISASSEMBLE_FUNCTION
-	if(jitCoder->flags & IL_CODER_FLAG_STATS)
-	{
-		if(!jit_function_compile(func))
-		{
-			return JIT_RESULT_COMPILE_ERROR;
-		}
-		ILMutexLock(globalTraceMutex);
-		jit_dump_function(stdout, func, methodName);
-		ILMutexUnlock(globalTraceMutex);
-	}
-#endif
-#endif
 	return JIT_RESULT_OK;
 }
 
